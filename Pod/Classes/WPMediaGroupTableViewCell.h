e7ffc5248094784bc10b358918b43fead604e71c
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-02-19 13:01:07 +0000

    Remove header from file.

--- Pod/Classes/WPMediaGroupTableViewCell.h
+++ Pod/Classes/WPMediaGroupTableViewCell.h
@@ -1,11 +1,3 @@
-//
-//  WPMediaGroupTableViewCell.h
-//  Pods
-//
-//  Created by Sergio Estevao on 19/02/2015.
-//
-//
-
 #import <UIKit/UIKit.h>
 
 @interface WPMediaGroupTableViewCell : UITableViewCell
