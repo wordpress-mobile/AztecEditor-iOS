83b5f8ee5b00eebe96b8b299b2dc4f1f84156255
Sérgio Estêvão <sergio.estevao@automattic.com>
2015-05-25 12:03:34 +1000

    Customise background color of cell.

--- Pod/Classes/WPMediaPicker.h
+++ Pod/Classes/WPMediaPicker.h
@@ -3,5 +3,6 @@
 #import "WPMediaPickerViewController.h"
 #import "WPMediaCollectionViewController.h"
 #import "WPMediaCollectionDataSource.h"
+#import "WPMediaCollectionViewCell.h"
 
 #endif /* _WPMEDIAPICKER_ */
