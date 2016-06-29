867d819271bc9aa939230849b02a912e3a54d363
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-06-29 11:42:08 -0300

    Improves the parser.

--- WordPress-Aztec-iOS/Classes/HTMLToNSAttributedString.swift
+++ WordPress-Aztec-iOS/Classes/HTMLToNSAttributedString.swift
@@ -1,16 +1,37 @@
 import Foundation
 import libxml2
 
-public class HTMLTag {
+public class HTMLTag: CustomDebugStringConvertible {
+    let name: String
+    private(set) var attributes = [Attribute]()
+
+    init(name: String) {
+        self.name = name
+    }
+
+    public var debugDescription: String {
+        get {
+            return "<\(String(self.dynamicType)): {name: \(name); attributes: \(String(attributes))}>"
+        }
+    }
+}
+
+extension HTMLTag {
 
     /// Represents a basic attribute with no value.  This is also the base class for all other
     /// attributes.
     ///
-    class Attribute {
+    class Attribute: CustomDebugStringConvertible {
         let name: String
 
         init(name: String) {
             self.name = name
+        }
+
+        public var debugDescription: String {
+            get {
+                return "<\(String(self.dynamicType)): {name: \(name)}>"
+            }
         }
     }
 
@@ -26,14 +47,12 @@ public class HTMLTag {
 
             super.init(name: name)
         }
-    }
 
-    let name: String
-    let attributes: [Attribute]?
-
-    init(name: String, attributes: [Attribute]?) {
-        self.name = name
-        self.attributes = attributes
+        override public var debugDescription: String {
+            get {
+                return "<\(String(self.dynamicType)): {name: \(name); value: \(value)}>"
+            }
+        }
     }
 }
 
@@ -44,6 +63,7 @@ public class HTMLToNSAttributedString {
 
     public func convert(html: NSData) -> NSAttributedString {
 
+        let result = NSMutableAttributedString()
         let bufferSize = 1024
         let buffer = Array<Int8>(count: bufferSize, repeatedValue: 0)
         let htmlPtr = UnsafePointer<Int8>(html.bytes)
@@ -70,58 +90,43 @@ public class HTMLToNSAttributedString {
 
         if rootElementPtr != nil {
             let rootElement = rootElementPtr.memory
-            let rootElementName = String(CString: UnsafePointer<Int8>(rootElement.name), encoding: NSUTF8StringEncoding)
-            NSLog("Root element name: \(rootElementName)")
+
+            guard let rootElementName = String(CString: UnsafePointer<Int8>(rootElement.name), encoding: NSUTF8StringEncoding) else {
+                // We should evaluate how to improve this condition check... is a nil value
+                // possible at all here?  If so... do we want to interrupt the parsing or try to
+                // recover from it?
+                //
+                // For the sake of moving forward I'm just interrupting here, but this could change
+                // if we find a unit test causing a nil value here.
+                //
+                fatalError("The root element name should not be nil.")
+            }
+
+            let tag = HTMLTag(name: rootElementName)
 
             var property = rootElement.properties
 
             while (property != nil) {
-                NSLog("Property: \(String(CString: UnsafePointer<Int8>(property.memory.name), encoding: NSUTF8StringEncoding))")
+                guard let propertyName = String(CString: UnsafePointer<Int8>(property.memory.name), encoding: NSUTF8StringEncoding) else {
+                    // We should evaluate how to improve this condition check... is a nil value
+                    // possible at all here?  If so... do we want to interrupt the parsing or try to
+                    // recover from it?
+                    //
+                    // For the sake of moving forward I'm just interrupting here, but this could change
+                    // if we find a unit test causing a nil value here.
+                    //
+                    fatalError("The root element name should not be nil.")
+                }
+
+                let attribute = HTMLTag.Attribute(name: propertyName)
+                tag.attributes.append(attribute)
+
                 property = property.memory.next
             }
+
+            result.addAttribute("HTMLTag", value: tag, range: NSRange(location: 0, length: 0))
         }
 
-        return NSAttributedString(string: "")
-/*
-        let htmlPtr = UnsafePointer<Int8>(html.bytes)
-
-        xmlResetLastError()
-
-        let options = Int32(XML_PARSE_NONET.rawValue)
-        let _ = xmlReadMemory(htmlPtr, Int32(html.length), "", nil, options)
-
-        let errorPtr = xmlGetLastError()
-
-        if errorPtr != nil {
-            let messagePtr = errorPtr.memory.message
-
-            if messagePtr != nil {
-                let message = String(CString: messagePtr, encoding: NSUTF8StringEncoding)
-
-                DDLogSwift.logDebug(message)
-            }
-        }
-
-        return NSAttributedString(string: "")
-*/
-
-        /*
-        let parserDelegate = ParserDelegate()
-        let parser = NSXMLParser(data: html)
-        parser.delegate = parserDelegate
-
-        if parser.parse() {
-            return parserDelegate.output
-        } else {
-
-        }
-
-        let attributedString = NSMutableAttributedString(string: "Hello there!")
-
-        let attribute = HTMLTag(name: "HTML", attributes: nil)
-        attributedString.addAttribute("HTMLTag", value: attribute, range: NSRange(location: 0, length: attributedString.length))
-
-        return attributedString
- */
+        return result
     }
 }
