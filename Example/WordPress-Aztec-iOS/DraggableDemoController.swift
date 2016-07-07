cd79109a769418e8401e2c309cfc856837e7ff76
aerych <aerychj@gmail.com>
2016-07-06 21:24:21 -0500

    Sets the stage for a draggable attachment demo.

--- Example/WordPress-Aztec-iOS/DraggableDemoController.swift
+++ Example/WordPress-Aztec-iOS/DraggableDemoController.swift
@@ -10,9 +10,10 @@ class DraggableDemoController: UIViewController
 
     @IBOutlet var textView: UITextView!
 
-    var editor: AztecTextEditor!
+    var attachmentManager: AztecAttachmentManager!
+    var attachmentViewList = [String: UIView]()
 
-    
+
     ///
     ///
     class func controller() -> DraggableDemoController {
@@ -24,22 +25,73 @@ class DraggableDemoController: UIViewController
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        configureEditor()
+        textView.layoutManager.delegate = self
+        textView.delegate = self
+        textView.attributedText = buildAttributedString()
+        attachmentManager = AztecAttachmentManager(textView: textView, delegate: self)
+
     }
 
 
-    func configureEditor() {
-        editor = AztecTextEditor(textView: textView)
-        if let filePath = NSBundle.mainBundle().URLForResource("SampleText", withExtension: "rtf"),
-            let attrStr = try? NSAttributedString(fileURL: filePath, options: [:], documentAttributes: nil) {
 
-//            editor.storage.appendAttributedString(attrStr)
-            textView.attributedText = attrStr
+    func buildAttributedString() -> NSAttributedString {
+        let lipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n"
+        let attachment1 = AztecTextAttachment(identifier: "foo")
+        let attachment2 = AztecTextAttachment(identifier: "bar")
+
+        let attributes = [
+            NSParagraphStyleAttributeName : NSParagraphStyle.defaultParagraphStyle(),
+            NSFontAttributeName: UIFont.systemFontOfSize(16)
+        ]
+
+        let attrStr = NSMutableAttributedString()
+        attrStr.appendAttributedString(NSAttributedString(string: lipsum, attributes: attributes))
+        attrStr.appendAttributedString(NSAttributedString(attachment: attachment1))
+        attrStr.appendAttributedString(NSAttributedString(string: lipsum, attributes: attributes))
+        attrStr.appendAttributedString(NSAttributedString(attachment: attachment2))
+        attrStr.appendAttributedString(NSAttributedString(string: lipsum, attributes: attributes))
+        attrStr.appendAttributedString(NSAttributedString(string: lipsum, attributes: attributes))
+        attrStr.appendAttributedString(NSAttributedString(string: lipsum, attributes: attributes))
+
+        return NSAttributedString(attributedString: attrStr)
+    }
+
+}
+
+
+extension DraggableDemoController : UITextViewDelegate
+{
+    func textViewDidChange(textView: UITextView) {
+        attachmentManager.updateAttachmentLayout()
+    }
+}
+
+
+extension DraggableDemoController : NSLayoutManagerDelegate
+{
+    func layoutManager(layoutManager: NSLayoutManager, textContainer: NSTextContainer, didChangeGeometryFromSize oldSize: CGSize) {
+        attachmentManager.resizeAttachments()
+    }
+}
+
+
+extension DraggableDemoController : AztecAttachmentManagerDelegate
+{
+
+    func attachmentManager(attachmentManager: AztecAttachmentManager, viewForAttachment attachment: AztecTextAttachment) -> UIView? {
+        if let attachmentView = attachmentViewList[attachment.identifier] {
+            return attachmentView
         }
+
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 50))
+        label.text = "Example Attachment View"
+        label.textAlignment = .Center
+        label.backgroundColor = UIColor.lightGrayColor()
+        label.font = UIFont.systemFontOfSize(20)
+
+        attachmentViewList[attachment.identifier] = label
+
+        return label
     }
-
-
-
-    
 
 }
