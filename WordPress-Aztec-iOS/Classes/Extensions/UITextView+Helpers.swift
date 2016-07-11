a6360d1c5ede7eb9d3f03788ff2901fb1794d929
aerych <aerychj@gmail.com>
2016-07-11 16:51:42 -0500

    Adds some documentation.

--- WordPress-Aztec-iOS/Classes/Extensions/UITextView+Helpers.swift
+++ WordPress-Aztec-iOS/Classes/Extensions/UITextView+Helpers.swift
@@ -1,11 +1,13 @@
 import UIKit
 
-///
+/// A helper extension for UITextView.
 ///
 extension UITextView
 {
 
+    /// A convenience metod for getting the CGRect of the currentl text selection.
     ///
+    /// - Returns: The CGRect of the current text selection.
     ///
     public func rectForCurrentSelection() -> CGRect {
         return layoutManager.boundingRectForGlyphRange(selectedRange, inTextContainer: textContainer)
