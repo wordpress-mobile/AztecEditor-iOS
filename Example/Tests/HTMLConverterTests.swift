0a3f4a78d9ed6dccee27ba496758c1d7ddc86f7d
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-07-12 15:09:27 -0300

    Adds a complex unit test.

--- Example/Tests/HTMLConverterTests.swift
+++ Example/Tests/HTMLConverterTests.swift
@@ -3,6 +3,12 @@ import XCTest
 
 class HTMLConverterTests: XCTestCase {
 
+    typealias ElementNode = Libxml2.HTML.ElementNode
+    typealias TextNode = Libxml2.HTML.TextNode
+
+    typealias Attribute = Libxml2.HTML.Attribute
+    typealias StringAttribute = Libxml2.HTML.StringAttribute
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -28,14 +34,52 @@ class HTMLConverterTests: XCTestCase {
     }
  */
 
-    func testExample2() {
+    func testComplexHTMLConversion() {
         let parser = Libxml2.In.HTMLConverter()
 
-        let html = "<HTML style='a' nostye peace='123'>Hello <b>World</b>!</HTML>"
+        let html = "<HTML styLe='a' nostyle peace='123'>Hello <b>World</b>!</HTML>"
         let htmlData = html.dataUsingEncoding(NSUTF8StringEncoding)!
 
         do {
-            _ = try parser.convert(htmlData)
+            let node = try parser.convert(htmlData)
+
+            guard let htmlNode = node as? ElementNode else {
+                XCTFail("Expected an element node.")
+                return
+            }
+
+            XCTAssertEqual(htmlNode.name, "html")
+            XCTAssertEqual(htmlNode.attributes.count, 3)
+
+            guard let attribute1 = htmlNode.attributes[0] as? StringAttribute else {
+                XCTFail("Expected a string attribute.")
+                return
+            }
+
+            let attribute2 = htmlNode.attributes[1]
+
+            guard let attribute3 = htmlNode.attributes[2] as? StringAttribute else {
+                XCTFail("Expected a string attribute.")
+                return
+            }
+
+            XCTAssertEqual(attribute1.name, "style")
+            XCTAssertEqual(attribute1.value, "a")
+            XCTAssertEqual(attribute2.name, "nostyle")
+            XCTAssertEqual(attribute3.name, "peace")
+            XCTAssertEqual(attribute3.value, "123")
+
+            XCTAssert(htmlNode.children[0] is TextNode)
+            XCTAssert(htmlNode.children[2] is TextNode)
+
+            guard let boldNode = htmlNode.children[1] as? ElementNode else {
+                XCTFail("Expected an element node.")
+                return
+            }
+
+            XCTAssert(boldNode.name == "b")
+            XCTAssert(boldNode.children[0] is TextNode)
+
         } catch {
             XCTFail("Unexpected conversion failure.")
         }
