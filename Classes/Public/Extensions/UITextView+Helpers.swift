0803bd85a95634a866a3112c8b08ba3999a293f4
Jorge Leandro Perez <jorge@lantean.co>
2016-08-24 11:48:02 -0300

    UITextView+Helpers: Adds Cursor Helper

--- Classes/Public/Extensions/UITextView+Helpers.swift
+++ Classes/Public/Extensions/UITextView+Helpers.swift
@@ -11,4 +11,17 @@ extension UITextView
     public func rectForCurrentSelection() -> CGRect {
         return layoutManager.boundingRectForGlyphRange(selectedRange, inTextContainer: textContainer)
     }
+
+
+    /// Determines what's the position of the cursor, expressed as the character index we're currently at.
+    ///
+    /// - Returns: An integer indicating the current position.
+    ///
+    public func positionForCursor() -> Int {
+        guard let selectedRange = selectedTextRange else {
+            return 0
+        }
+
+        return offsetFromPosition(beginningOfDocument, toPosition: selectedRange.start)
+    }
 }
