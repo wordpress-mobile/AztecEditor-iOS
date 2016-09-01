f3c922d6049059b9f3772fdbf280e7c9ca03d1eb
Jorge Leandro Perez <jorge@lantean.co>
2016-09-01 13:20:09 -0300

    AztecAttachmentManager: Above and Below Line Clearing

--- Classes/Public/TextKit/AztecAttachmentManager.swift
+++ Classes/Public/TextKit/AztecAttachmentManager.swift
@@ -449,7 +449,7 @@ public class AztecTextAttachment: NSTextAttachment
 
     /// Wrapping Mode
     ///
-    public var textWrapping = TextWrapping.Around
+    public var textWrapping = TextWrapping.AboveAndBelow
 
 
     /// Designed Initializer
@@ -462,5 +462,24 @@ public class AztecTextAttachment: NSTextAttachment
     required public init?(coder aDecoder: NSCoder) {
         self.identifier = ""
         super.init(coder: aDecoder)
+    }
+
+
+    /// Overriden Methods
+    ///
+    public override func attachmentBoundsForTextContainer(textContainer: NSTextContainer?, proposedLineFragment lineFrag: CGRect, glyphPosition position: CGPoint, characterIndex charIndex: Int) -> CGRect {
+        switch textWrapping {
+        case .AboveAndBelow:
+            return aboveAndBelowBoundsForTextContainer(textContainer, proposedLineFragment: lineFrag, glyphPosition: position)
+        case .Around:
+            return super.attachmentBoundsForTextContainer(textContainer, proposedLineFragment: lineFrag, glyphPosition: position, characterIndex: charIndex)
+        }
+    }
+
+    private func aboveAndBelowBoundsForTextContainer(textContainer: NSTextContainer?, proposedLineFragment lineFrag: CGRect, glyphPosition position: CGPoint) -> CGRect {
+        let width = (lineFrag.width - position.x) - (textContainer!.lineFragmentPadding * 2.0)
+        let rect = CGRectMake(0.0, 0.0, floor(width), 1.0)
+
+        return rect
     }
 }
