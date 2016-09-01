ee6455501112903059ad71d2d3b7d5142a7efd6f
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-09-01 12:46:11 -0400

    Add support for HTML comment nodes.

---
+++ Classes/Private/Libxml2/Data/CommentNode.swift
@@ -0,0 +1,93 @@
+import Foundation
+
+extension Libxml2 {
+    /// Comment nodes use to hold HTML comments like this: <!-- This is a comment -->
+    ///
+    class CommentNode: Node {
+
+        var text: String
+
+        init(text: String) {
+            self.text = text
+
+            super.init(name: "text")
+        }
+
+        override func customMirror() -> Mirror {
+            return Mirror(self, children: ["type": "text", "name": name, "text": text, "parent": parent.debugDescription], ancestorRepresentation: .Suppressed)
+        }
+
+        /// Node length.
+        ///
+        override func length() -> Int {
+            return text.characters.count
+        }
+
+        override func deleteCharacters(inRange range: NSRange) {
+
+            guard let textRange = text.rangeFromNSRange(range) else {
+                fatalError("The specified range is out of bounds.")
+            }
+
+            text.removeRange(textRange)
+        }
+
+        override func replaceCharacters(inRange range: NSRange, withString string: String) {
+
+            guard let textRange = text.rangeFromNSRange(range) else {
+                fatalError("The specified range is out of bounds.")
+            }
+
+            text.replaceRange(textRange, with: string)
+        }
+
+        override func split(forRange range: NSRange) {
+
+            guard let swiftRange = text.rangeFromNSRange(range) else {
+                fatalError("This scenario should not be possible. Review the logic.")
+            }
+
+            guard let parent = parent,
+                let nodeIndex = parent.children.indexOf(self) else {
+
+                fatalError("This scenario should not be possible. Review the logic.")
+            }
+
+            let preRange = text.startIndex ..< swiftRange.startIndex
+            let postRange = swiftRange.endIndex ..< text.endIndex
+
+            if postRange.count > 0 {
+                let newNode = TextNode(text: text.substringWithRange(postRange))
+
+                text.removeRange(postRange)
+                parent.insert(newNode, at: nodeIndex + 1)
+            }
+
+            if preRange.count > 0 {
+                let newNode = TextNode(text: text.substringWithRange(preRange))
+
+                text.removeRange(preRange)
+                parent.insert(newNode, at: nodeIndex)
+            }
+        }
+
+
+        /// Wraps the specified range inside a node with the specified name.
+        ///
+        /// - Parameters:
+        ///     - targetRange: the range that must be wrapped.
+        ///     - nodeName: the name of the node to wrap the range in.
+        ///     - attributes: the attributes the wrapping node will have when created.
+        ///
+        override func wrap(range targetRange: NSRange, inNodeNamed nodeName: String, withAttributes attributes: [Attribute]) {
+
+            guard !NSEqualRanges(targetRange, NSRange(location: 0, length: length())) else {
+                wrap(inNodeNamed: nodeName, withAttributes: attributes)
+                return
+            }
+
+            split(forRange: targetRange)
+            wrap(inNodeNamed: nodeName, withAttributes: attributes)
+        }
+    }
+}
