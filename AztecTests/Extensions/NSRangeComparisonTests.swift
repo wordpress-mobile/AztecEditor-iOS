a32b34637dd93bee08cdd56af8d03c76684c4ee4
Jorge Leandro Perez <jorge@lantean.co>
2016-10-03 14:53:04 -0300

    Updates NSRangeComparisonTests

--- AztecTests/Extensions/NSRangeComparisonTests.swift
+++ AztecTests/Extensions/NSRangeComparisonTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import Aztec
 
-class NSRange_ComparisonTests: XCTestCase {
+class NSRangeComparisonTests: XCTestCase {
     
     /// Tests that `intersect(withRange:)` works.
     ///
