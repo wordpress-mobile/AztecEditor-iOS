7a5b6ca02fa86b76f4f53e82dc042f1e873f9154
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-10-05 14:56:02 +0100

    Replace the attachment character instead of wrapping it.

--- Aztec/Classes/Public/TextKit/TextStorage.swift
+++ Aztec/Classes/Public/TextKit/TextStorage.swift
@@ -296,9 +296,9 @@ public class TextStorage: NSTextStorage {
     }
 
     func insertImage(url: NSURL, forRange range: NSRange) {
-        rootNode.wrap(range: range,
-                      inNodeNamed: ElementTypes.img.rawValue,
-                      withAttributes: [Libxml2.StringAttribute(name:"src", value: url.absoluteString!)])
+        rootNode.replace(range: range,
+                         withNodeNamed: ElementTypes.img.rawValue,
+                         withAttributes: [Libxml2.StringAttribute(name:"src", value: url.absoluteString!)])
     }
 
     private func toggleAttribute(attributeName: String, value: AnyObject, range: NSRange, onEnable: (NSRange) -> Void, onDisable: (NSRange) -> Void) {
