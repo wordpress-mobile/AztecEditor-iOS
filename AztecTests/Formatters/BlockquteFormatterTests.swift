014147a1e96268a430d8fe9129c277cbdfd80326
Jorge Bernal <jbernal@gmail.com>
2016-10-27 16:40:10 +0200

    Fix unit tests

--- AztecTests/Formatters/BlockquteFormatterTests.swift
+++ AztecTests/Formatters/BlockquteFormatterTests.swift
@@ -1,66 +1,72 @@
 import XCTest
+import Gridicons
 @testable import Aztec
 
 class BlockquteFormatterTests: XCTestCase {
     func testApplyingBlockquoteOnFirstParagraph() {
-        let string = NSMutableAttributedString(string: plainText)
-        let paragraphs = paragraphRanges(inString: string)
+        let textView = testTextView
+        let paragraphs = paragraphRanges(inString: textView.storage)
 
         let formatter = BlockquoteFormatter()
-        formatter.toggleAttribute(inString: string, atRange: NSRange(location: 1, length: 1))
+        formatter.toggleAttribute(inTextView: textView, atRange: NSRange(location: 1, length: 1))
 
-        XCTAssertTrue(existsBlockquote(for: string, in: paragraphs[0]))
+        XCTAssertTrue(existsBlockquote(for: textView.storage, in: paragraphs[0]))
     }
 
     func testRemovingBlockQuoteOnFirstParagraph() {
-        let string = NSMutableAttributedString(string: plainText)
-        let paragraphs = paragraphRanges(inString: string)
+        let textView = testTextView
+        let paragraphs = paragraphRanges(inString: textView.storage)
 
         let formatter = BlockquoteFormatter()
-        string.setAttributes(formatter.attributes, range: paragraphs[0])
-        formatter.toggleAttribute(inString: string, atRange: NSRange(location: 1, length: 1))
+        textView.storage.setAttributes(formatter.attributes, range: paragraphs[0])
+        formatter.toggleAttribute(inTextView: textView, atRange: NSRange(location: 1, length: 1))
 
-        XCTAssertFalse(existsBlockquote(for: string, in: paragraphs[0]))
+        XCTAssertFalse(existsBlockquote(for: textView.storage, in: paragraphs[0]))
     }
 
     func testApplyingBlockquoteOnFirstParagraphWhenSecondHasBlockquote() {
-        let string = NSMutableAttributedString(string: plainText)
-        let paragraphs = paragraphRanges(inString: string)
+        let textView = testTextView
+        let paragraphs = paragraphRanges(inString: textView.storage)
 
         let formatter = BlockquoteFormatter()
-        string.setAttributes(formatter.attributes, range: paragraphs[1])
-        formatter.toggleAttribute(inString: string, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
+        textView.storage.setAttributes(formatter.attributes, range: paragraphs[1])
+        formatter.toggleAttribute(inTextView: textView, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
 
-        XCTAssertTrue(existsBlockquote(for: string, in: paragraphs[0]))
-        XCTAssertTrue(existsBlockquote(for: string, in: paragraphs[1]))
+        XCTAssertTrue(existsBlockquote(for: textView.storage, in: paragraphs[0]))
+        XCTAssertTrue(existsBlockquote(for: textView.storage, in: paragraphs[1]))
     }
 
     func testRemovingBlockQuoteOnFirstParagraphWhenSecondDoesNotHaveBlockquote() {
-        let string = NSMutableAttributedString(string: plainText)
-        let paragraphs = paragraphRanges(inString: string)
+        let textView = testTextView
+        let paragraphs = paragraphRanges(inString: textView.storage)
 
         let formatter = BlockquoteFormatter()
-        string.setAttributes(formatter.attributes, range: paragraphs[0])
-        formatter.toggleAttribute(inString: string, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
+        textView.storage.setAttributes(formatter.attributes, range: paragraphs[0])
+        formatter.toggleAttribute(inTextView: textView, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
 
-        XCTAssertFalse(existsBlockquote(for: string, in: paragraphs[0]))
-        XCTAssertFalse(existsBlockquote(for: string, in: paragraphs[1]))
+        XCTAssertFalse(existsBlockquote(for: textView.storage, in: paragraphs[0]))
+        XCTAssertFalse(existsBlockquote(for: textView.storage, in: paragraphs[1]))
     }
 
     func testToggleBlockquoteTwiceLeavesReturnsIdenticalString() {
-        let string = NSMutableAttributedString(string: plainText)
-        let paragraphs = paragraphRanges(inString: string)
+        let textView = testTextView
+        let paragraphs = paragraphRanges(inString: textView.storage)
 
         let formatter = BlockquoteFormatter()
-        let original = string.copy() as! NSAttributedString
-        formatter.toggleAttribute(inString: string, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
-        formatter.toggleAttribute(inString: string, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
+        let original = textView.storage.copy() as! NSAttributedString
+        formatter.toggleAttribute(inTextView: textView, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
+        formatter.toggleAttribute(inTextView: textView, atRange: NSUnionRange(paragraphs[0], paragraphs[1]))
 
-        XCTAssertTrue(original.isEqualToAttributedString(string))
+        XCTAssertTrue(original.isEqualToAttributedString(textView.storage))
     }
 }
 
 private extension BlockquteFormatterTests {
+    var testTextView: TextView {
+        let view = TextView(defaultFont: UIFont.systemFontOfSize(14), defaultMissingImage: Gridicon.iconOfType(.Image))
+        view.text = plainText
+        return view
+    }
     var plainText: String {
         return plainTextParagraphLines.joinWithSeparator("")
     }
