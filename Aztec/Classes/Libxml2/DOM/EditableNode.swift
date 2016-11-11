2b6f63f09d7863b2b3ecc8240245ffaf8f4fb64f
Diego Rey Mendez <diegoreymendez@gmail.com>
2016-11-11 04:04:22 -0300

    Removes some unnecessary spaces.

--- Aztec/Classes/Libxml2/DOM/EditableNode.swift
+++ Aztec/Classes/Libxml2/DOM/EditableNode.swift
@@ -20,6 +20,6 @@ protocol EditableNode {
     ///     - location: the text location to split the node at.
     ///
     func split(atLocation location: Int)
-    func split(forRange range: NSRange)    
+    func split(forRange range: NSRange)
     func wrap(range targetRange: NSRange, inElement elementDescriptor: Libxml2.ElementNodeDescriptor)
 }
