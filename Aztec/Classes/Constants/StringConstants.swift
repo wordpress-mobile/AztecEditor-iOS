2123ff40ce3cf1698fa0f141d69547cc8c8bdaaa
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-01-23 11:06:58 -0300

    De-lanteaning code.

--- Aztec/Classes/Constants/StringConstants.swift
+++ Aztec/Classes/Constants/StringConstants.swift
@@ -4,6 +4,6 @@ import Foundation
 /// Collection of String Helpers
 ///
 struct StringConstants {
-    static let zeroWidthSpace   = "\u{200B}"
-    static let newline          = "\n"
+    static let newline = "\n"
+    static let zeroWidthSpace = "\u{200B}"
 }
