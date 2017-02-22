127da02fe145255e520004333bad15112e98fc18
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-02-22 12:34:09 -0300

    Adds a unit test to prevent text node fragmentation.

--- AztecTests/HTML/ElementNodeTests.swift
+++ AztecTests/HTML/ElementNodeTests.swift
@@ -1323,6 +1323,20 @@ class ElementNodeTests: XCTestCase {
         XCTAssertEqual(paragraph.children[1], boldNode)
     }
 
+    // MARK: - replaceCharactersInRange
+
+    func testInsertingTextDoesntFragmentTextNodes() {
+        let textNode = TextNode(text: "")
+        let paragraphNode = ElementNode(name: StandardElementType.p.rawValue, attributes: [], children: [textNode])
+
+        paragraphNode.replaceCharacters(inRange: NSRange(location: 0, length: 0), withString: "a")
+        paragraphNode.replaceCharacters(inRange: NSRange(location: 1, length: 0), withString: "b")
+        paragraphNode.replaceCharacters(inRange: NSRange(location: 2, length: 0), withString: "c")
+
+        XCTAssertEqual(paragraphNode.children.count, 1)
+        XCTAssert(paragraphNode.children[0] is TextNode)
+    }
+
     /// Tests `replaceCharacters(inRange:withString:)`.
     ///
     /// Input HTML: `<p>Click on this <a href="http://www.wordpress.com">link</a></p>`
