47f4e3e03a427d19df52876fffc41d8d8ad32398
Sergio Estevao <sergioestevao@gmail.com>
2017-03-09 14:31:46 +0000

    Improve gesture handling by only using the UITextView gesture if no attachment are present.

--- Example/Example/EditorDemoController.swift
+++ Example/Example/EditorDemoController.swift
@@ -25,6 +25,12 @@ class EditorDemoController: UIViewController {
         textView.mediaDelegate = self
         textView.addGestureRecognizer(self.tapGestureRecognizer)
 
+        for gesture in textView.gestureRecognizers ?? [] {
+            if let otherTapGesture = gesture as? UITapGestureRecognizer, otherTapGesture != self.tapGestureRecognizer {
+                otherTapGesture.require(toFail: self.tapGestureRecognizer)
+            }
+        }
+
         return textView
     }()
 
@@ -90,7 +96,9 @@ class EditorDemoController: UIViewController {
 
     fileprivate(set) lazy var tapGestureRecognizer: UIGestureRecognizer = {
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(richTextViewWasPressed))
-        recognizer.cancelsTouchesInView = false
+        recognizer.cancelsTouchesInView = true
+        recognizer.delaysTouchesBegan = true
+        recognizer.delaysTouchesEnded = true
         recognizer.delegate = self
         return recognizer
     }()
@@ -818,13 +826,27 @@ extension EditorDemoController: UIGestureRecognizerDelegate
         return true
     }
 
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        let locationInTextView = gestureRecognizer.location(in: richTextView)
+        // check if we have an attachment in the position we tapped
+        guard richTextView.attachmentAtPoint(locationInTextView) != nil else {
+            // if we have a current selected attachment marked lets unmark it
+            if let selectedAttachment = currentSelectedAttachment {
+                selectedAttachment.clearAllOverlays()
+                richTextView.refreshLayoutFor(attachment: selectedAttachment)
+                currentSelectedAttachment = nil
+            }
+            return false
+        }
+        return true
+    }
+
     func richTextViewWasPressed(_ recognizer: UIGestureRecognizer) {
         let locationInTextView = recognizer.location(in: richTextView)
         // check if we have an attachment in the position we tapped
         guard let attachment = richTextView.attachmentAtPoint(locationInTextView) else {
             // if we have an attachment marked lets unmark it
             if let selectedAttachment = currentSelectedAttachment {
-                selectedAttachment.message = nil
                 selectedAttachment.clearAllOverlays()
                 richTextView.refreshLayoutFor(attachment: selectedAttachment)
                 currentSelectedAttachment = nil
@@ -839,7 +861,7 @@ extension EditorDemoController: UIGestureRecognizerDelegate
         } else {
             // if it's a new attachment tapped let unmark the previous one
             if let selectedAttachment = currentSelectedAttachment {
-                selectedAttachment.message = nil
+                selectedAttachment.clearAllOverlays()
                 richTextView.refreshLayoutFor(attachment: selectedAttachment)
             }
             // and mark the newly tapped attachment
