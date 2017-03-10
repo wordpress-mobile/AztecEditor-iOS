6bc1b176938bee0fb0cc827efcad5ceada702d42
Jorge Leandro Perez <jorge@lantean.co>
2017-03-10 11:31:45 -0300

    FormatBar: Private Helpers Extension

--- Aztec/Classes/GUI/FormatBar/FormatBar.swift
+++ Aztec/Classes/GUI/FormatBar/FormatBar.swift
@@ -106,3 +106,98 @@ open class FormatBar: UIView {
         formatter?.handleActionForIdentifier(sender.identifier!)
     }
 }
+
+
+
+// MARK: - Private Helpers
+//
+private extension FormatBar {
+
+    /// Detaches a given collection of FormatBarItem's
+    ///
+    func detach(items: [FormatBarItem]) {
+        for item in items {
+            item.removeFromSuperview()
+        }
+    }
+
+
+    /// Sets up a given collection of FormatBarItem's1
+    ///
+    func configure(items: [FormatBarItem]) {
+        for item in items {
+            configure(item: item)
+        }
+    }
+
+
+    /// Sets up a given FormatBarItem
+    ///
+    func configure(item: FormatBarItem) {
+        item.tintColor = tintColor
+        item.selectedTintColor = selectedTintColor
+        item.highlightedTintColor = highlightedTintColor
+        item.disabledTintColor = disabledTintColor
+
+        item.addTarget(self, action: #selector(handleButtonAction), for: .touchUpInside)
+    }
+
+
+    /// Sets up a given StackView
+    ///
+    func configure(stackView: UIStackView) {
+        stackView.axis = .horizontal
+        stackView.spacing = Constants.stackViewSpacing
+        stackView.alignment = .center
+        stackView.distribution = .equalCentering
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+
+    /// Sets up the ScrollView
+    ///
+    func configure(scrollView: UIScrollView) {
+        scrollView.isScrollEnabled = true
+        scrollView.alwaysBounceHorizontal = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+    }
+
+
+    /// Sets up the Constraints
+    ///
+    func configureConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            scrollView.topAnchor.constraint(equalTo: topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: fixedStackView.leadingAnchor, constant: -1 * Constants.fixedLeftPadding)
+            ])
+
+        NSLayoutConstraint.activate([
+            fixedStackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -1 * Constants.edgesSpacing),
+            fixedStackView.topAnchor.constraint(equalTo: topAnchor),
+            fixedStackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            ])
+
+        NSLayoutConstraint.activate([
+            scrollableStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: Constants.edgesSpacing),
+            scrollableStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -1 * Constants.edgesSpacing),
+            scrollableStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            scrollableStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            scrollableStackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor),
+            scrollableStackView.widthAnchor.constraint(greaterThanOrEqualTo: scrollView.widthAnchor, constant: -2 *  Constants.edgesSpacing)
+            ])
+    }
+}
+
+
+// MARK: - Private Constants
+//
+private extension FormatBar {
+
+    struct Constants {
+        static let edgesSpacing = CGFloat(10)
+        static let fixedLeftPadding = CGFloat(10)
+        static let stackViewSpacing = CGFloat(7)
+    }
+}
