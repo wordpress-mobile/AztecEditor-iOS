c554e443e71286d01cbb60b2a458d5447d345479
Sergio Estevao <sergioestevao@gmail.com>
2017-03-15 14:14:28 +0000

    Fix crashes and missing updates when deleting around headers and other paragraph formatters.

--- Aztec/Classes/TextKit/TextView.swift
+++ Aztec/Classes/TextKit/TextView.swift
@@ -488,7 +488,18 @@ open class TextView: UITextView {
         forceRedrawCursorAfterDelay()
     }
 
-
+    private lazy var paragraphFormatters: [AttributeFormatter] = [
+        TextListFormatter(style: .ordered),
+        TextListFormatter(style: .unordered),
+        BlockquoteFormatter(),
+        HeaderFormatter(headerLevel:.h1),
+        HeaderFormatter(headerLevel:.h2),
+        HeaderFormatter(headerLevel:.h3),
+        HeaderFormatter(headerLevel:.h4),
+        HeaderFormatter(headerLevel:.h5),
+        HeaderFormatter(headerLevel:.h6),
+    ]
+    
     /// After text deletion, this helper will re-apply the Text Formatters at the specified range, if they were
     /// present in the segment previous to the modified range.
     ///
@@ -500,10 +511,12 @@ open class TextView: UITextView {
         guard deletedText.string == String(.newline) || range.location == 0 else {
             return
         }
-        let formatters:[AttributeFormatter] = [TextListFormatter(style: .ordered), TextListFormatter(style: .unordered), BlockquoteFormatter(), HeaderFormatter(headerLevel:.h1),HeaderFormatter(headerLevel:.h2), HeaderFormatter(headerLevel:.h3)]
-        for formatter in formatters {
-            if range.location > 0 && formatter.present(in: textStorage, at: range.location-1) {
-                formatter.applyAttributes(to: storage, at: range)
+        for formatter in paragraphFormatters {
+            if let locationBefore = storage.string.location(before: range.location),
+                formatter.present(in: textStorage, at: locationBefore) {
+                if range.endLocation < storage.length {
+                    formatter.applyAttributes(to: storage, at: range)
+                }
             } else if formatter.present(in: textStorage, at: range.location) || range.location == 0 {
                 formatter.removeAttributes(from: textStorage, at: range)
             }
