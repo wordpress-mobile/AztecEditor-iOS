ecc5c36f97dcac1676fd9de61e97b898ad1fff94
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-03-21 15:03:37 -0300

    Added a new test for the newline deletion before lists.

--- AztecTests/TextViewTests.swift
+++ AztecTests/TextViewTests.swift
@@ -484,6 +484,37 @@ class AztecVisualTextViewTests: XCTestCase {
         XCTAssertEqual(textView.getHTML(), "<p>HelloWorld!</p>")
     }
 
+    /// Tests that deleting a newline works by merging the component around it.
+    ///
+    /// Input:
+    ///     - Initial HTML: "List<ul><li>first</li><li>second</li>"
+    ///     - Deletion range: (loc: 4, len 1)
+    ///     - Second deletion range: (loc: 9, len: 1)
+    ///
+    /// Output:
+    ///     - Final HTML: "Listfirstsecond"
+    ///
+    func testDeleteNewline5() {
+
+        let textView = createTextView(withHTML: "List<ul><li>first</li><li>second</li>")
+
+        let rangeStart = textView.position(from: textView.beginningOfDocument, offset: 4)!
+        let rangeEnd = textView.position(from: rangeStart, offset: 1)!
+        let range = textView.textRange(from: rangeStart, to: rangeEnd)!
+
+        textView.replace(range, withText: "")
+
+        XCTAssertEqual(textView.getHTML(), "Listfirst<ul><li>second</li></ul>")
+
+        let rangeStart2 = textView.position(from: textView.beginningOfDocument, offset: 9)!
+        let rangeEnd2 = textView.position(from: rangeStart2, offset: 1)!
+        let range2 = textView.textRange(from: rangeStart2, to: rangeEnd2)!
+
+        textView.replace(range2, withText: "")
+
+        XCTAssertEqual(textView.getHTML(), "Listfirstsecond")
+    }
+
     /// Tests that deleting a newline works at the end of text with paragraph with header before works.
     ///
     /// Input:
