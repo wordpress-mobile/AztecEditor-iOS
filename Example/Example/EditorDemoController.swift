cf54ed6d35bcb832c323f78f6c600e10dd17b9f6
Sergio Estevao <sergioestevao@gmail.com>
2017-04-03 13:01:11 +0100

    Update scrolling method.

--- Example/Example/EditorDemoController.swift
+++ Example/Example/EditorDemoController.swift
@@ -50,6 +50,7 @@ class EditorDemoController: UIViewController {
         textField.textColor = UIColor.darkText
         textField.translatesAutoresizingMaskIntoConstraints = false
         textField.isScrollEnabled = false
+        textField.backgroundColor = .clear
         let toolbar = self.createToolbar(htmlMode: false)
         toolbar.enabled = false
         textField.inputAccessoryView = toolbar
@@ -120,12 +121,11 @@ class EditorDemoController: UIViewController {
         navigationController?.navigationBar.isTranslucent = false
 
         view.backgroundColor = UIColor.white
+        view.addSubview(richTextView)
+        view.addSubview(htmlTextView)
         view.addSubview(titleTextField)
         view.addSubview(titlePlaceholderLabel)
         view.addSubview(separatorView)
-        view.addSubview(richTextView)
-        view.addSubview(htmlTextView)
-
         configureConstraints()
 
         let html: String
@@ -166,22 +166,29 @@ class EditorDemoController: UIViewController {
 
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
-        // TODO: Update toolbars
-        //    [self.editorToolbar configureForHorizontalSizeClass:newCollection.horizontalSizeClass];
-        //    [self.titleToolbar configureForHorizontalSizeClass:newCollection.horizontalSizeClass];
-
     }
 
     // MARK: - Title and Title placeholder position methods
     func updateTitlePosition() {
         let referenceView: UIScrollView = editingMode == .richText ? richTextView : htmlTextView
-        titleTopConstraint.constant = -min(titleHeightConstraint.constant + separatorView.frame.height + (Constants.margin / 2), referenceView.contentOffset.y)
+        titleTopConstraint.constant = -(referenceView.contentOffset.y+referenceView.contentInset.top)
+
+        var contentInset = referenceView.contentInset
+        contentInset.top = (titleHeightConstraint.constant + separatorView.frame.height)
+        referenceView.contentInset = contentInset
     }
     
     func updateTitleHeight() {
+        let referenceView: UIScrollView = editingMode == .richText ? richTextView : htmlTextView
+
         let sizeThatShouldFitTheContent = titleTextField.sizeThatFits(CGSize(width:view.frame.width - ( 2 * Constants.margin), height: CGFloat.greatestFiniteMagnitude))
         let insets = titleTextField.textContainerInset
         titleHeightConstraint.constant = max(sizeThatShouldFitTheContent.height, titleTextField.font!.lineHeight + insets.top + insets.bottom)
+
+        var contentInset = referenceView.contentInset
+        contentInset.top = (titleHeightConstraint.constant + separatorView.frame.height)
+        referenceView.contentInset = contentInset
+        referenceView.setContentOffset(CGPoint(x:0, y: -contentInset.top), animated: false)
     }
 
     func updateTitlePlaceholderVisibility() {
@@ -220,7 +227,7 @@ class EditorDemoController: UIViewController {
         NSLayoutConstraint.activate([
             richTextView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: Constants.margin),
             richTextView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -Constants.margin),
-            richTextView.topAnchor.constraint(equalTo: separatorView.bottomAnchor, constant: Constants.margin / 2),
+            richTextView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
             richTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -Constants.margin)
             ])
 
@@ -285,8 +292,10 @@ class EditorDemoController: UIViewController {
     }
 
     fileprivate func refreshInsets(forKeyboardFrame keyboardFrame: CGRect) {
-        let scrollInsets = UIEdgeInsets(top: 0, left: 0, bottom: view.frame.maxY - keyboardFrame.minY, right: 0)
-        let contentInset = UIEdgeInsets(top: 0, left: 0, bottom: view.frame.maxY - keyboardFrame.minY, right: 0)
+        let referenceView: UIScrollView = editingMode == .richText ? richTextView : htmlTextView
+
+        let scrollInsets = UIEdgeInsets(top: referenceView.scrollIndicatorInsets.top, left: 0, bottom: view.frame.maxY - keyboardFrame.minY, right: 0)
+        let contentInset = UIEdgeInsets(top: referenceView.contentInset.top, left: 0, bottom: view.frame.maxY - keyboardFrame.minY, right: 0)
 
         htmlTextView.scrollIndicatorInsets = scrollInsets
         htmlTextView.contentInset = contentInset
