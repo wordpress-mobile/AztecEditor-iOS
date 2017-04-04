83e8ae7539699ead9b31128ece20fde8a922d34e
Jorge Leandro Perez <jorge@lantean.co>
2017-04-04 10:44:03 -0300

    Implements HTMLAttachment

---
+++ Aztec/Classes/TextKit/HTMLAttachment.swift
@@ -0,0 +1,54 @@
+import Foundation
+import UIKit
+
+
+/// HTML Attachments: Represents unknown HTML
+///
+open class HTMLAttachment: NSTextAttachment {
+
+    /// Internal Cached Image
+    ///
+    fileprivate var glyphImage: UIImage?
+
+    /// Delegate
+    ///
+    weak var delegate: RenderableAttachmentDelegate?
+
+    /// Name of the Root "Unknown" Tag
+    ///
+    open var rootTagName: String = "" {
+        didSet {
+            glyphImage = nil
+        }
+    }
+
+    /// Raw Unknown HTML to be rendered
+    ///
+    open var rawHTML: String = "" {
+        didSet {
+            glyphImage = nil
+        }
+    }
+
+
+    // MARK: - NSTextAttachmentContainer
+
+    override open func image(forBounds imageBounds: CGRect, textContainer: NSTextContainer?, characterIndex charIndex: Int) -> UIImage? {
+        if let cachedImage = glyphImage, imageBounds.size.equalTo(cachedImage.size) {
+            return cachedImage
+        }
+
+        glyphImage = delegate?.attachment(self, imageForSize: imageBounds.size)
+
+        return glyphImage
+    }
+
+    override open func attachmentBounds(for textContainer: NSTextContainer?, proposedLineFragment lineFrag: CGRect, glyphPosition position: CGPoint, characterIndex charIndex: Int) -> CGRect {
+        guard let bounds = delegate?.attachment(self, boundsForLineFragment: lineFrag) else {
+            assertionFailure("Could not determine HTML Attachment Size")
+            return .zero
+        }
+
+        return bounds
+    }
+}
