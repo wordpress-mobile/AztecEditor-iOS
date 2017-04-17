c2cb0d3ba409671750a8c3c0699f311765672a20
Jorge Leandro Perez <jorge@lantean.co>
2017-04-17 11:27:25 -0300

    TextListFormatter: Adds comments

--- Aztec/Classes/Formatters/TextListFormatter.swift
+++ Aztec/Classes/Formatters/TextListFormatter.swift
@@ -1,53 +1,75 @@
 import Foundation
 import UIKit
 
+
+// MARK: - Lists Formatter
+//
 class TextListFormatter: ParagraphAttributeFormatter {
-    
+
+    /// Style of the list
+    ///
     let listStyle: TextList.Style
+
+    /// Attributes to be added by default
+    ///
     let placeholderAttributes: [String : Any]?
 
+
+    /// Designated Initializer
+    ///
     init(style: TextList.Style, placeholderAttributes: [String : Any]? = nil) {
         self.listStyle = style
         self.placeholderAttributes = placeholderAttributes
     }
 
+
+    // MARK: - Overwriten Methods
+
     func apply(to attributes: [String : Any]) -> [String: Any] {
-        var resultingAttributes = attributes
         let newParagraphStyle = ParagraphStyle()
         if let paragraphStyle = attributes[NSParagraphStyleAttributeName] as? NSParagraphStyle {
             newParagraphStyle.setParagraphStyle(paragraphStyle)
         }
+
         if newParagraphStyle.textList == nil {
             newParagraphStyle.headIndent += Metrics.listTextIndentation
             newParagraphStyle.firstLineHeadIndent += Metrics.listTextIndentation
         }
+
         newParagraphStyle.textList = TextList(style: self.listStyle)
+
+        var resultingAttributes = attributes
         resultingAttributes[NSParagraphStyleAttributeName] = newParagraphStyle
+
         return resultingAttributes
     }
 
-    func remove(from attributes:[String: Any]) -> [String: Any] {
-        var resultingAttributes = attributes
-        let newParagraphStyle = ParagraphStyle()
-        guard let paragraphStyle = attributes[NSParagraphStyleAttributeName] as? ParagraphStyle,
-            paragraphStyle.textList?.style == self.listStyle
-        else {
-            return resultingAttributes
+    func remove(from attributes: [String: Any]) -> [String: Any] {
+        guard present(in: attributes) else {
+            return attributes
         }
-        newParagraphStyle.setParagraphStyle(paragraphStyle)
+
+        let newParagraphStyle = ParagraphStyle()
+        if let paragraphStyle = attributes[NSParagraphStyleAttributeName] as? NSParagraphStyle {
+            newParagraphStyle.setParagraphStyle(paragraphStyle)
+        }
         newParagraphStyle.headIndent -= Metrics.listTextIndentation
         newParagraphStyle.firstLineHeadIndent -= Metrics.listTextIndentation
         newParagraphStyle.textList = nil
+
+        var resultingAttributes = attributes
         resultingAttributes[NSParagraphStyleAttributeName] = newParagraphStyle
+
         return resultingAttributes
     }
 
     func present(in attributes: [String : Any]) -> Bool {
-        guard let paragraphStyle = attributes[NSParagraphStyleAttributeName] as? ParagraphStyle,
-              let textList = paragraphStyle.textList else {
+        guard let style = attributes[NSParagraphStyleAttributeName] as? ParagraphStyle, let list = style.textList else {
             return false
         }
-        return textList.style == listStyle
+
+        return list.style == listStyle
+    }
     }
 }
 
