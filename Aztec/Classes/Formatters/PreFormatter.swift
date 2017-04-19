9a2cebd37803e4e69cd4a1cd0d942bca20a45522
Jorge Leandro Perez <jorge@lantean.co>
2017-04-19 17:13:48 -0300

    PreFormatter: Adds Comments

--- Aztec/Classes/Formatters/PreFormatter.swift
+++ Aztec/Classes/Formatters/PreFormatter.swift
@@ -1,15 +1,29 @@
 import Foundation
 import UIKit
 
+
+// MARK: - Pre Formatter
+//
 open class PreFormatter: ParagraphAttributeFormatter {
 
+    /// Font to be used
+    ///
     let monospaceFont: UIFont
+
+    /// Attributes to be added by default
+    ///
     let placeholderAttributes: [String : Any]?
 
+
+    /// Designated Initializer
+    ///
     init(monospaceFont: UIFont = UIFont(descriptor:UIFontDescriptor(name: "Courier", size: 12), size:12), placeholderAttributes: [String : Any]? = nil) {
         self.monospaceFont = monospaceFont
         self.placeholderAttributes = placeholderAttributes
     }
+
+
+    // MARK: - Overwriten Methods
 
     func apply(to attributes: [String : Any]) -> [String: Any] {
         var resultingAttributes = attributes
@@ -35,10 +49,7 @@ open class PreFormatter: ParagraphAttributeFormatter {
     }
 
     func present(in attributes: [String : Any]) -> Bool {
-        if let font = attributes[NSFontAttributeName] as? UIFont {
-            return font == monospaceFont
-        }
-        return false
+        let font = attributes[NSFontAttributeName] as? UIFont
+        return font == monospaceFont
     }
 }
-
