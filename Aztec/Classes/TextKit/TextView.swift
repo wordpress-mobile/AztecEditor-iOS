8e9953b5ad34d5b18f07155ef309ffc24cd0b4e9
Jorge Leandro Perez <jorge@lantean.co>
2017-04-20 15:35:43 -0300

    TextView: Nukes comment

--- Aztec/Classes/TextKit/TextView.swift
+++ Aztec/Classes/TextKit/TextView.swift
@@ -292,7 +292,6 @@ open class TextView: UITextView {
         ///
         ensureInsertionOfNewline(beforeInserting: text)
 
-        // Note:
         // Whenever the entered text causes the Paragraph Attributes to be removed, we should prevent the actual
         // text insertion to happen. Thus, we won't call super.insertText.
         // But because we don't call the super we need to refresh the attributes ourselfs, and callback to the delegate.
