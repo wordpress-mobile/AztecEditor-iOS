f73cee9871d74be09272f42b8fe54e8ef1b534bc
Jorge Leandro Perez <jorge@lantean.co>
2017-05-03 17:34:00 -0300

    Libtidy: Cleanup

--- Aztec/Classes/Libtidy/Libtidy.swift
+++ Aztec/Classes/Libtidy/Libtidy.swift
@@ -1,16 +1,13 @@
 import Foundation
 import libtidy
 
+
+// MARK: -
+//
 open class Libtidy {
 
-    ///
-    ///
-    private let Success = yes
 
-    ///
-    ///
-    private let utf8 = "utf8".cString(using: .utf8)
-
+    // MARK: - Initializers
 
     public init() {
 
@@ -24,188 +21,134 @@ open class Libtidy {
             return nil
         }
 
+        var output: String?
         var buffer = TidyBuffer()
         tidyBufInit(UnsafeMutablePointer<TidyBuffer>(&buffer))
 
-        defer {
-            tidyBufFree(UnsafeMutablePointer<TidyBuffer>(&buffer))
-            tidyRelease(document)
+        do {
+            try setup(document: document, buffer: &buffer)
+            try parse(string: input, document: document)
+            output = try export(document: document)
+        } catch {
+            NSLog("Tidy Error: \(error)")
         }
 
-        guard tidyOptSetBool(document, TidyXmlTags, yes) == Success else {
-            return nil
-        }
+        tidyBufFree(UnsafeMutablePointer<TidyBuffer>(&buffer))
+        tidyRelease(document)
 
-        guard tidyOptSetBool(document, TidyXmlOut, yes) == Success else {
-            return nil
-        }
-
-        guard tidyOptSetBool(document, TidyForceOutput, yes) == Success else {
-            return nil
-        }
-
-        guard tidyOptSetInt(document, TidyIndentContent, UInt(TidyAutoState.rawValue)) == Success else {
-            return nil
-        }
-
-        guard tidySetInCharEncoding(document, utf8) == 0 else {
-            return nil
-        }
-
-        guard tidySetOutCharEncoding(document, utf8) == 0 else {
-            return nil
-        }
-
-        guard tidySetErrorBuffer(document, UnsafeMutablePointer<TidyBuffer>(&buffer)) == 0 else {
-            return nil
-        }
-
-        guard let inString = input.data(using: .utf8) else {
-            return nil
-        }
-
-
-        let parseResult = inString.withUnsafeBytes { (inString: UnsafePointer<Int8>) -> Int32 in
-            return tidyParseString(document, inString)
-        }
-
-//        let test =         NSString(bytes: buffer.bp, length: Int(buffer.size), encoding: String.Encoding.utf8.rawValue)
-//        NSLog("Error: \(test)")
-
-        guard parseResult >= 0 else {
-            return nil
-        }
-
-
-        //        if (outError)
-        //        {
-        //            NSDictionary *theUserInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-        //                                         [NSString stringWithUTF8String:(char *)theErrorBuffer.bp], NSLocalizedDescriptionKey,
-        //                                         NULL];
-        //            *outError = [NSError errorWithDomain:@"TODO_DOMAIN" code:theResultCode userInfo:theUserInfo];
-        //        }
-        //        return(NULL);
-
-        guard tidyCleanAndRepair(document) >= 0 else {
-            return nil
-        }
-
-        guard tidyRunDiagnostics(document) >= 0 else {
-            return nil
-        }
-
-        var length = uint(0)
-        tidySaveString(document, nil, UnsafeMutablePointer<uint>(&length) )
-
-        guard let mutableData = NSMutableData(length: Int(length)) else {
-            return nil
-        }
-
-        let data = mutableData.mutableBytes.bindMemory(to: Int8.self, capacity: Int(length))
-
-        guard tidySaveString(document, data, UnsafeMutablePointer<uint>(&length)) >= 0 else {
-            return nil
-        }
-
-        guard let string = String(data: mutableData as Data, encoding: .utf8) else {
-            return nil
-        }
-
-        //    if (theErrorBuffer.bp != NULL)
-        //    {
-        //        NSData *theErrorData = [NSData dataWithBytes:theErrorBuffer.bp length:theErrorBuffer.size];
-        ////        *outDiagnostics = [[NSString alloc] initWithData:theErrorData encoding:NSUTF8StringEncoding];
-        //    }
-
-
-        return string
+        return output
     }
 }
 
 
+// MARK: -
+//
+private extension Libtidy {
+
+    ///
+    ///
+    func setup(document: TidyDoc, buffer: inout TidyBuffer) throws {
+        guard tidyOptSetBool(document, TidyXmlTags, yes) == yes else {
+            throw TidySetupErrors.xmlTag
+        }
+
+        guard tidyOptSetBool(document, TidyXmlOut, yes) == yes else {
+            throw TidySetupErrors.xmlOut
+        }
+
+        guard tidyOptSetBool(document, TidyForceOutput, yes) == yes else {
+            throw TidySetupErrors.output
+        }
+
+        guard tidyOptSetInt(document, TidyIndentContent, UInt(TidyAutoState.rawValue)) == yes else {
+            throw TidySetupErrors.indent
+        }
+
+        guard tidySetInCharEncoding(document, TidyConstants.utf8) >= 0 else {
+            throw TidySetupErrors.encoding
+        }
+
+        guard tidySetOutCharEncoding(document, TidyConstants.utf8) >= 0 else {
+            throw TidySetupErrors.encoding
+        }
+
+        guard tidySetErrorBuffer(document, UnsafeMutablePointer<TidyBuffer>(&buffer)) >= 0 else {
+            throw TidySetupErrors.buffer
+        }
+    }
+
+    ///
+    ///
+    func parse(string: String, document: TidyDoc) throws {
+        guard let utf8String = string.data(using: .utf8) else {
+            throw TidyPrettifyErrors.parse
+        }
+
+        let result = utf8String.withUnsafeBytes { (inString: UnsafePointer<Int8>) -> Int32 in
+            return tidyParseString(document, inString)
+        }
+
+        guard result >= 0 else {
+            throw TidyPrettifyErrors.parse
+        }
+
+        guard tidyCleanAndRepair(document) >= 0 else {
+            throw TidyPrettifyErrors.clean
+        }
+
+        guard tidyRunDiagnostics(document) >= 0 else {
+            throw TidyPrettifyErrors.diagnostics
+        }
+    }
+
+    ///
+    ///
+    func export(document: TidyDoc) throws -> String? {
+        var length = uint(0)
+        tidySaveString(document, nil, UnsafeMutablePointer<uint>(&length) )
+
+        guard let mutableData = NSMutableData(length: Int(length)) else {
+            throw TidyExportErrors.alloc
+        }
+
+        let dataAsInt8 = mutableData.mutableBytes.bindMemory(to: Int8.self, capacity: Int(length))
+        guard tidySaveString(document, dataAsInt8, UnsafeMutablePointer<uint>(&length)) >= 0 else {
+            throw TidyExportErrors.save
+        }
+
+        return String(data: mutableData as Data, encoding: .utf8)
+    }
+}
 
 
-//- (NSString *)tidyString:(NSString *)inString encoding:(NSStringEncoding)inEncoding error:(NSError **)outError
-//{
-//    TidyDoc theTidyDocument = tidyCreate();
+// MARK: -
 //
-//    int theResultCode = 0;
-//
-//    // Set input format if input is XML (xhtml & html are the tidy 'default')
-//    theResultCode = tidyOptSetBool(theTidyDocument, TidyXmlTags, YES);
-//    NSAssert(theResultCode >= 0, @"tidyOptSetBool() should return 0");
-//
-//    // Set output format
-//    TidyOptionId theOutputValue = TidyXmlOut;
-//    theResultCode = tidyOptSetBool(theTidyDocument, theOutputValue, YES);
-//    NSAssert(theResultCode >= 0, @"tidyOptSetBool() should return 0");
-//
-//    // Force output even if errors found
-//    theResultCode = tidyOptSetBool(theTidyDocument, TidyForceOutput, YES);
-//    NSAssert(theResultCode >= 0, @"tidyOptSetBool() should return 0");
-//
-//    // Pretty!
-//    tidyOptSetInt(theTidyDocument, TidyIndentContent, TidyAutoState);
-//
-//    // Set encoding - same for input and output
-//    theResultCode = tidySetInCharEncoding(theTidyDocument, "utf8");
-//    NSAssert(theResultCode >= 0, @"tidySetInCharEncoding() should return 0");
-//    theResultCode = tidySetOutCharEncoding(theTidyDocument, "utf8");
-//    NSAssert(theResultCode >= 0, @"tidySetOutCharEncoding() should return 0");
-//
-//    // Create an error buffer
-//    TidyBuffer theErrorBuffer;
-//    tidyBufInit(&theErrorBuffer);
-//    theResultCode = tidySetErrorBuffer(theTidyDocument, &theErrorBuffer);
-//    NSAssert(theResultCode >= 0, @"tidySetErrorBuffer() should return 0");
-//
-//    // #############################################################################
-//
-//    // Parse the data.
-//    theResultCode = tidyParseString(theTidyDocument, [inString UTF8String]);
-//    if (theResultCode < 0)
-//    {
-//        if (outError)
-//        {
-//            NSDictionary *theUserInfo = [NSDictionary dictionaryWithObjectsAndKeys:
-//                                         [NSString stringWithUTF8String:(char *)theErrorBuffer.bp], NSLocalizedDescriptionKey,
-//                                         NULL];
-//            *outError = [NSError errorWithDomain:@"TODO_DOMAIN" code:theResultCode userInfo:theUserInfo];
-//        }
-//        return(NULL);
-//    }
-//
-//    // Repair the data
-//    theResultCode = tidyCleanAndRepair(theTidyDocument);
-//    if (theResultCode < 0)
-//    {
-//        return(NULL);
-//    }
-//
-//    //theResultCode = tidyRunDiagnostics(theTidyDocument);
-//
-//    //
-//    uint theBufferLength = 0;
-//
-//    theResultCode = tidySaveString(theTidyDocument, NULL, &theBufferLength);
-//
-//    NSMutableData *theOutputBuffer = [NSMutableData dataWithLength:theBufferLength];
-//
-//    theResultCode = tidySaveString(theTidyDocument, [theOutputBuffer mutableBytes], &theBufferLength);
-//
-//    NSString *theString = [[NSString alloc] initWithData:theOutputBuffer encoding:NSUTF8StringEncoding];
-//
-//    //
-//    if (theErrorBuffer.bp != NULL)
-//    {
-//        NSData *theErrorData = [NSData dataWithBytes:theErrorBuffer.bp length:theErrorBuffer.size];
-////        *outDiagnostics = [[NSString alloc] initWithData:theErrorData encoding:NSUTF8StringEncoding];
-//    }
-//    tidyBufFree(&theErrorBuffer);
-//
-//    // #############################################################################
-//
-//    tidyRelease(theTidyDocument);
-//
-//    return(theString);
-//}
+extension Libtidy {
+
+    struct TidyConstants {
+        static let utf8 = "utf8".cString(using: .utf8)
+    }
+
+    enum TidyExportErrors: Error {
+        case alloc
+        case save
+    }
+
+    enum TidyPrettifyErrors: Error {
+        case parse
+        case clean
+        case diagnostics
+    }
+
+    enum TidySetupErrors: Error {
+        case xmlTag
+        case xmlOut
+        case indent
+        case encoding
+        case output
+        case buffer
+    }
+}
+
+// let test = NSString(bytes: buffer.bp, length: Int(buffer.size), encoding: String.Encoding.utf8.rawValue)
+// NSLog("Error: \(test)")
