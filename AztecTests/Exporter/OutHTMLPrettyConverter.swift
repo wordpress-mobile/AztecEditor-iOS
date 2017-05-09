3d6674f25e8bb8a2df02ae04f9978f59443a14c8
Jorge Leandro Perez <jorge@lantean.co>
2017-05-09 12:35:48 -0300

    Renames OutHTMLPrettyConverterTests

--- AztecTests/Exporter/OutHTMLPrettyConverter.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-class OutHTMLPrettyConverter: XCTestCase {
-
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-}
---
+++ AztecTests/Exporter/OutHTMLPrettyConverterTests.swift
@@ -0,0 +1,49 @@
+import XCTest
+
+class OutHTMLPrettyConverterTests: XCTestCase {
+
+///
+///
+//    func testPrettifyUnclosedTagDoesNotCauseDataLoss() {
+//        let sample = "test<br>"
+//        let expected = "test<br />\n"
+//
+//        XCTAssertEqual(tidy.prettify(html: sample), expected)
+//    }
+//
+//    ///
+//    ///
+//    func testPrettifyListCorrectlyReturnsFormattedList() {
+//        let sample = "<ul><li>Item</li><li>Item</li></ul>"
+//        let expected = "<ul>\n<li>Item</li>\n<li>Item</li>\n</ul>"
+//
+//        XCTAssertEqual(tidy.prettify(html: sample), expected)
+//    }
+//
+//    ///
+//    ///
+//    func testPrettifyDoesNotDropProprietaryTags() {
+//        let sample = "something something <something>"
+//        let expected = "something something <something>"
+//
+//        XCTAssertEqual(tidy.prettify(html: sample), expected)
+//    }
+//
+//    ///
+//    ///
+//    func testPrettifyDoesNotDropProprietaryAttributes() {
+//        let sample = "something something <img unknown='true' />"
+//        let expected =  "something something <img unknown='true' />"
+//
+//        XCTAssertEqual(tidy.prettify(html: sample), expected)
+//    }
+//
+//    ///
+//    ///
+//    func testPrettifyDoesNotAttemptToCloseUnclosedTags() {
+//        let sample = "<h1><i>italic heading</h1>"
+//        let expected = "<h1>\n<i>italic heading</i>\n</h1>"
+//
+//        XCTAssertEqual(tidy.prettify(html: sample), expected)
+//    }
+}
