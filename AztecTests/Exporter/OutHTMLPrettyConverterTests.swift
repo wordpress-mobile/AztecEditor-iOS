0fa5386858b7f5cf7f21b7ace13ab4ea98ad02f7
Jorge Leandro Perez <jorge@lantean.co>
2017-05-10 16:48:06 -0300

    OutHTMLPrettyConverterTests: More Tests!

--- AztecTests/Exporter/OutHTMLPrettyConverterTests.swift
+++ AztecTests/Exporter/OutHTMLPrettyConverterTests.swift
@@ -9,51 +9,128 @@ class OutHTMLPrettyConverterTests: XCTestCase {
     let inputConverter = Libxml2.In.HTMLConverter()
     let outputConverter = Libxml2.Out.HTMLPrettyConverter()
 
+
+    override func setUp() {
+        super.setUp()
+        outputConverter.prettyPrintEnabled = false
+    }
+
     ///
     ///
-    func testPrettifyUnclosedTagDoesNotCauseDataLoss() {
+    func testConverterProperlyIndentsSingleLevelList() {
         let sample = "<ul><li>Item</li><li>Item</li></ul>"
         let expected = "<ul>\n  <li>Item</li>\n  <li>Item</li>\n</ul>"
 
-        let node = try! inputConverter.convert(sample)
+        do {
+            outputConverter.prettyPrintEnabled = true
 
-        outputConverter.prettyPrintEnabled = true
-        XCTAssertEqual(outputConverter.convert(node), expected)
+            let inNode = try inputConverter.convert(sample)
+            let outHtml = outputConverter.convert(inNode)
+
+            XCTAssertEqual(outHtml, expected)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
     }
 
     ///
     ///
-    func testPrettifyDoesNotDropUnknownTags() {
+    func testConverterProperlyIndentsMultilevelList() {
+        let sample = "<ul><li><ol><li>Nested 1</li><li>Nested 2</li></ol></li><li>Regular</li></ul>"
+        let expected = "<ul>\n  <li>\n    <ol>\n      <li>Nested 1</li>\n      <li>Nested 2</li>\n    </ol>\n  </li>\n  <li>Regular</li>\n</ul>"
+
+        do {
+            outputConverter.prettyPrintEnabled = true
+
+            let inNode = try inputConverter.convert(sample)
+            let outHtml = outputConverter.convert(inNode)
+
+            XCTAssertEqual(outHtml, expected)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
+    }
+
+
+    ///
+    ///
+    func testConverterDoesNotDropUnknownTags() {
         let sample = "something something <something>"
         let expected = "something something <something></something>"
 
-        let node = try! inputConverter.convert(sample)
+        do {
+            outputConverter.prettyPrintEnabled = true
 
-        outputConverter.prettyPrintEnabled = true
-        XCTAssertEqual(outputConverter.convert(node), expected)
+            let inNode = try inputConverter.convert(sample)
+            let outHtml = outputConverter.convert(inNode)
+
+            XCTAssertEqual(outHtml, expected)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
     }
 
     ///
     ///
-    func testPrettifyDoesNotDropProprietaryAttributes() {
+    func testConverterDoesNotDropProprietaryAttributes() {
         let sample = "something something <img unknown=\"true\">"
         let expected =  "something something <img unknown=\"true\">"
 
-        let node = try! inputConverter.convert(sample)
+        do {
+            outputConverter.prettyPrintEnabled = true
 
-        outputConverter.prettyPrintEnabled = true
-        XCTAssertEqual(outputConverter.convert(node), expected)
+            let inNode = try inputConverter.convert(sample)
+            let outHtml = outputConverter.convert(inNode)
+
+            XCTAssertEqual(outHtml, expected)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
     }
 
     ///
     ///
-    func testPrettifyDoesNotAttemptToCloseUnclosedTags() {
+    func testConverterAddsClosingTagWhenMissing() {
         let sample = "<h1><i>italic heading</h1>"
         let expected = "<h1><i>italic heading</i></h1>"
 
-        let node = try! inputConverter.convert(sample)
+        do {
+            let inNode = try inputConverter.convert(sample)
+            let outHtml = outputConverter.convert(inNode)
 
-        outputConverter.prettyPrintEnabled = true
-        XCTAssertEqual(outputConverter.convert(node), expected)
+            XCTAssertEqual(outHtml, expected)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
+    }
+
+    ///
+    ///
+    func testConverterProperlySerializesImportedHtml() {
+        let html = "<bold><i>Hello!</i></bold>"
+
+        do {
+            let inNode = try inputConverter.convert(html)
+            let outHtml = outputConverter.convert(inNode)
+
+            XCTAssertEqual(outHtml, html)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
+    }
+
+    ///
+    ///
+    func testConverterProperlySerializesImportedHtmlWithComments() {
+        let html = "<!--Hello Sample--><bold><i>Hello!</i></bold>"
+
+        do {
+            let inNode = try inputConverter.convert(html)
+            let outHtml = outputConverter.convert(inNode)
+
+            XCTAssertEqual(outHtml, html)
+        } catch {
+            XCTFail("Unexpected conversion failure.")
+        }
     }
 }
