21e90d45727a4c6933120a9ce1cdde2031613392
Jorge Leandro Perez <jorge@lantean.co>
2017-05-10 16:30:50 -0300

    OutHTMLPrettyConverter: New Unit Tests

--- AztecTests/Exporter/OutHTMLPrettyConverterTests.swift
+++ AztecTests/Exporter/OutHTMLPrettyConverterTests.swift
@@ -1,50 +1,59 @@
 import XCTest
+@testable import Aztec
 
 
 // MARK: - OutHTMLPrettyConverterTests
 //
 class OutHTMLPrettyConverterTests: XCTestCase {
-//
-//    func testPrettifyUnclosedTagDoesNotCauseDataLoss() {
-//        let sample = "test<br>"
-//        let expected = "test<br />\n"
-//
-//        XCTAssertEqual(tidy.prettify(html: sample), expected)
-//    }
-//
-//    ///
-//    ///
-//    func testPrettifyListCorrectlyReturnsFormattedList() {
-//        let sample = "<ul><li>Item</li><li>Item</li></ul>"
-//        let expected = "<ul>\n<li>Item</li>\n<li>Item</li>\n</ul>"
-//
-//        XCTAssertEqual(tidy.prettify(html: sample), expected)
-//    }
-//
-//    ///
-//    ///
-//    func testPrettifyDoesNotDropProprietaryTags() {
-//        let sample = "something something <something>"
-//        let expected = "something something <something>"
-//
-//        XCTAssertEqual(tidy.prettify(html: sample), expected)
-//    }
-//
-//    ///
-//    ///
-//    func testPrettifyDoesNotDropProprietaryAttributes() {
-//        let sample = "something something <img unknown='true' />"
-//        let expected =  "something something <img unknown='true' />"
-//
-//        XCTAssertEqual(tidy.prettify(html: sample), expected)
-//    }
-//
-//    ///
-//    ///
-//    func testPrettifyDoesNotAttemptToCloseUnclosedTags() {
-//        let sample = "<h1><i>italic heading</h1>"
-//        let expected = "<h1>\n<i>italic heading</i>\n</h1>"
-//
-//        XCTAssertEqual(tidy.prettify(html: sample), expected)
-//    }
+
+    let inputConverter = Libxml2.In.HTMLConverter()
+    let outputConverter = Libxml2.Out.HTMLPrettyConverter()
+
+    ///
+    ///
+    func testPrettifyUnclosedTagDoesNotCauseDataLoss() {
+        let sample = "<ul><li>Item</li><li>Item</li></ul>"
+        let expected = "<ul>\n  <li>Item</li>\n  <li>Item</li>\n</ul>"
+
+        let node = try! inputConverter.convert(sample)
+
+        outputConverter.prettyPrintEnabled = true
+        XCTAssertEqual(outputConverter.convert(node), expected)
+    }
+
+    ///
+    ///
+    func testPrettifyDoesNotDropUnknownTags() {
+        let sample = "something something <something>"
+        let expected = "something something <something></something>"
+
+        let node = try! inputConverter.convert(sample)
+
+        outputConverter.prettyPrintEnabled = true
+        XCTAssertEqual(outputConverter.convert(node), expected)
+    }
+
+    ///
+    ///
+    func testPrettifyDoesNotDropProprietaryAttributes() {
+        let sample = "something something <img unknown=\"true\">"
+        let expected =  "something something <img unknown=\"true\">"
+
+        let node = try! inputConverter.convert(sample)
+
+        outputConverter.prettyPrintEnabled = true
+        XCTAssertEqual(outputConverter.convert(node), expected)
+    }
+
+    ///
+    ///
+    func testPrettifyDoesNotAttemptToCloseUnclosedTags() {
+        let sample = "<h1><i>italic heading</h1>"
+        let expected = "<h1><i>italic heading</i></h1>"
+
+        let node = try! inputConverter.convert(sample)
+
+        outputConverter.prettyPrintEnabled = true
+        XCTAssertEqual(outputConverter.convert(node), expected)
+    }
 }
