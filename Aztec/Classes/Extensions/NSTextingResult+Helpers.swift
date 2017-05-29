9a47f51e928540349ad336a90c877445828604ab
Sergio Estevao <sergioestevao@gmail.com>
2017-05-29 12:21:31 +0100

    Move processor generic code to the main Aztec project.

---
+++ Aztec/Classes/Extensions/NSTextingResult+Helpers.swift
@@ -0,0 +1,27 @@
+ import Foundation
+
+public extension NSTextCheckingResult {
+
+    /// Returns the match for the corresponding capture group position in a text
+    ///
+    /// - Parameters:
+    ///   - position: the capture group position
+    ///   - text: the string where the match was detected
+    /// - Returns: the string with the captured group text
+    ///
+    func captureGroup(in position: Int, text: String) -> String? {
+        guard position < numberOfRanges else {
+            return nil
+        }
+
+        let nsrange = rangeAt(position)
+
+        guard nsrange.location != NSNotFound else {
+            return nil
+        }
+
+        let range = text.range(from: nsrange)
+        let captureGroup = text.substring(with: range)
+        return captureGroup
+    }
+}
--- Example/Example/ShortcodeProcessor.swift
+++ Example/Example/ShortcodeProcessor.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Aztec
 /// Struct to represent a WordPress shortcode
 /// More details here: https://codex.wordpress.org/Shortcode and here: https://en.support.wordpress.com/shortcodes/
 ///
@@ -77,31 +78,5 @@ open class ShortcodeProcessor: RegexProcessor {
         }
 
         super.init(regex: regex, replacer: regexReplacer)
-    }
-}
-
-extension NSTextCheckingResult {
-
-    /// Returns the match for the corresponding capture group position in a text
-    ///
-    /// - Parameters:
-    ///   - position: the capture group position
-    ///   - text: the string where the match was detected
-    /// - Returns: the string with the captured group text
-    ///
-    func captureGroup(in position: Int, text: String) -> String? {
-        guard position < numberOfRanges else {
-            return nil
-        }
-
-        let nsrange = rangeAt(position)
-
-        guard nsrange.location != NSNotFound else {
-            return nil
-        }
-
-        let range = text.range(from: nsrange)
-        let captureGroup = text.substring(with: range)
-        return captureGroup
     }
 }
