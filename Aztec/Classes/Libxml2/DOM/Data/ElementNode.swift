25fffb8a608e68eb0762189d1cd3f741e5df3f69
Jorge Bernal <jbernal@gmail.com>
2017-06-07 06:38:14 +0200

    Fix build errors on Xcode 9

--- Aztec/Classes/Libxml2/DOM/Data/ElementNode.swift
+++ Aztec/Classes/Libxml2/DOM/Data/ElementNode.swift
@@ -1571,9 +1571,9 @@ extension Libxml2 {
             }
 
             let finalWrapper = wrapperElement ?? { () -> ElementNode in
-                let newNode = ElementNode(descriptor: elementDescriptor, children: childrenToWrap, editContext: editContext)
+                let newNode = ElementNode(descriptor: elementDescriptor, children: childrenToWrap, editContext: self.editContext)
 
-                children.insert(newNode, at: firstNodeIndex)
+                self.children.insert(newNode, at: firstNodeIndex)
                 newNode.parent = self
 
                 return newNode
