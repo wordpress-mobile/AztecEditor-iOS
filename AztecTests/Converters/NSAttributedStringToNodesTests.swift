692278c2bd3e249350d93d457b3b03686a890c46
Jorge Leandro Perez <jorge@lantean.co>
2017-06-09 15:17:43 -0300

    NSAttributedStringToNodesTests: Tests Mark II

--- AztecTests/Converters/NSAttributedStringToNodesTests.swift
+++ AztecTests/Converters/NSAttributedStringToNodesTests.swift
@@ -18,21 +18,41 @@ class NSAttributedStringToNodesTests: XCTestCase {
     /// Verifies that `<b>Bold?</b>` gets effectively translated into it's tree representation.
     ///
     func testBoldStyleGetsEffectivelyConvertedIntoBoldNode() {
-        let html = "<b>Bold?</b>"
+        let input = "<b>Bold?</b>"
+        let expected = input
 
-        let converter = NSAttributedStringToNodes()
-        let attrString = attributedString(from: html)
+        let attrString = attributedString(from: input)
+        let node = rootNode(from: attrString)
+        let generated = html(from: node)
 
-        let root = converter.convert(attrString)
-        XCTAssert(root.children.count == 1)
+        XCTAssertEqual(generated, expected)
+    }
 
-        guard let bold = root.children.first as? ElementNode, let text = bold.children.first as? TextNode else {
-            XCTFail()
-            return
-        }
+    ///
+    ///
+    func testListItemsGetEffectivelyMerged() {
+        let input = "<blockquote><ul><li>First Line</li></ul></blockquote><blockquote><ul><li>Second Line</li></ul></blockquote>"
+        let expected = "<blockquote><ul><li>First Line</li><li>Second Line</li></ul></blockquote>"
 
-        XCTAssert(bold.children.count == 1)
-        XCTAssertEqual(text.contents, "Bold?")
+        let attrString = attributedString(from: input)
+        let node = rootNode(from: attrString)
+        let generated = html(from: node)
+
+        XCTAssertEqual(generated, expected)
+    }
+
+    ///
+    ///
+    func testSomething() {
+        let input = "<ul><li><blockquote>text 1</blockquote></li></ul>" +
+                    "<ul><li><blockquote>text 2</blockquote></li></ul>"
+        let expected = "<ul><li><blockquote>text 1</blockquote><blockquote>text 2</blockquote></li></ul>"
+
+        let attrString = attributedString(from: input)
+        let node = rootNode(from: attrString)
+        let generated = html(from: node)
+
+        XCTAssertEqual(generated, expected)
     }
 }
 
@@ -49,5 +69,19 @@ private extension NSAttributedStringToNodesTests {
         let (_, attrString) = try! converter.convert(html)
 
         return attrString
+    }
+
+    ///
+    ///
+    func rootNode(from attrString: NSAttributedString) -> RootNode {
+        let converter = NSAttributedStringToNodes()
+        return converter.convert(attrString)
+    }
+
+    /// Converts a RootNode into it's HTML Representation
+    ///
+    func html(from node: RootNode) -> String {
+        let converter = Libxml2.Out.HTMLConverter()
+        return converter.convert(node)
     }
 }
