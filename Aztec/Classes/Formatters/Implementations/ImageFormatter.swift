b22b0f7ff1922a589fe4a061c70a4909dca21e9f
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-06-15 14:08:52 -0300

    Fixes images and video loading.

--- Aztec/Classes/Formatters/Implementations/ImageFormatter.swift
+++ Aztec/Classes/Formatters/Implementations/ImageFormatter.swift
@@ -4,8 +4,52 @@ class ImageFormatter: StandardAttributeFormatter {
     static let htmlRepresentationKey = "Image.htmlRepresentation"
 
     init() {
-        super.init(attributeKey: NSAttachmentAttributeName,
-                   attributeValue: ImageAttachment(identifier: NSUUID().uuidString),
-                   htmlRepresentationKey: ImageFormatter.htmlRepresentationKey)
+        super.init(
+            attributeKey: NSAttachmentAttributeName,
+            attributeValue: ImageAttachment(identifier: NSUUID().uuidString),
+            htmlRepresentationKey: ImageFormatter.htmlRepresentationKey)
+    }
+
+    override func apply(to attributes: [String : Any], andStore representation: HTMLRepresentation?) -> [String: Any] {
+        let elementRepresentation = representation as? HTMLElementRepresentation
+
+        guard representation == nil || elementRepresentation != nil else {
+            fatalError("This should not be possible.  Review the logic")
+        }
+
+        return apply(to: attributes, andStore: elementRepresentation)
+    }
+
+    func apply(to attributes: [String : Any], andStore representation: HTMLElementRepresentation?) -> [String: Any] {
+
+        if let representation = representation {
+            let url: URL?
+
+            if let urlString = representation.valueForAttribute(named: "src") {
+                url = URL(string: urlString)
+            } else {
+                url = nil
+            }
+
+            let attachment = ImageAttachment(identifier: UUID().uuidString, url: url)
+
+            if let elementClass = representation.valueForAttribute(named: "class") {
+                let classAttributes = elementClass.components(separatedBy: " ")
+                for classAttribute in classAttributes {
+                    if let alignment = ImageAttachment.Alignment.fromHTML(string: classAttribute) {
+                        attachment.alignment = alignment
+                    }
+                    if let size = ImageAttachment.Size.fromHTML(string: classAttribute) {
+                        attachment.size = size
+                    }
+                }
+            }
+
+            attributeValue = attachment
+        } else {
+            attributeValue = ImageAttachment(identifier: UUID().uuidString)
+        }
+
+        return super.apply(to: attributes, andStore: representation)
     }
 }
--- Aztec/Classes/Formatters/Implementations/VideoFormatter.swift
+++ Aztec/Classes/Formatters/Implementations/VideoFormatter.swift
@@ -5,7 +5,45 @@ class VideoFormatter: StandardAttributeFormatter {
 
     init() {
         super.init(attributeKey: NSAttachmentAttributeName,
-                   attributeValue: VideoAttachment(identifier: NSUUID().uuidString, namedAttributes: [String:String](), unnamedAttributes: []),
+                   attributeValue: VideoAttachment(identifier: NSUUID().uuidString),
                    htmlRepresentationKey: VideoFormatter.htmlRepresentationKey)
+    }
+
+    override func apply(to attributes: [String : Any], andStore representation: HTMLRepresentation?) -> [String: Any] {
+        let elementRepresentation = representation as? HTMLElementRepresentation
+
+        guard representation == nil || elementRepresentation != nil else {
+            fatalError("This should not be possible.  Review the logic")
+        }
+
+        return apply(to: attributes, andStore: elementRepresentation)
+    }
+
+    func apply(to attributes: [String : Any], andStore representation: HTMLElementRepresentation?) -> [String: Any] {
+
+        if let representation = representation {
+
+            let srcURL: URL?
+
+            if let urlString = representation.valueForAttribute(named: "src") {
+                srcURL = URL(string: urlString)
+            } else {
+                srcURL = nil
+            }
+
+            let posterURL: URL?
+
+            if let urlString = representation.valueForAttribute(named: "poster") {
+                posterURL = URL(string: urlString)
+            } else {
+                posterURL = nil
+            }
+
+            let attachment = VideoAttachment(identifier: UUID().uuidString, srcURL: srcURL, posterURL: posterURL)
+            
+            attributeValue = attachment
+        }
+
+        return super.apply(to: attributes, andStore: representation)
     }
 }
