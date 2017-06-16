e6663f1833d1a3be698889d79e6b8d3da3e123f4
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-06-16 12:57:36 -0300

    Improve undo support.

--- Example/Example/EditorDemoController.swift
+++ Example/Example/EditorDemoController.swift
@@ -443,6 +443,8 @@ extension EditorDemoController : UITextViewDelegate {
         case titleTextField:
             updateTitleHeight()
             updateTitlePlaceholderVisibility()
+        case richTextView:
+            updateFormatBar()
         default:
             break
         }
