13768f0f7a57a83dbd062589e917f33c2984da63
James Frost <james.frost@automattic.com>
2017-06-21 13:25:13 +0100

    FormatBarDelegate: Document methods.

--- Aztec/Classes/GUI/FormatBar/FormatBarDelegate.swift
+++ Aztec/Classes/GUI/FormatBar/FormatBarDelegate.swift
@@ -7,7 +7,17 @@ public enum FormatBarOverflowState {
 }
 
 public protocol FormatBarDelegate : NSObjectProtocol {
+    /// Prompts the delegate that the bar item with the specified identifier was tapped,
+    /// and it should take appropriate action.
+    ///
     func handleActionForIdentifier(_ identifier: FormattingIdentifier, barItem: FormatBarItem)
+
+    /// Informs the delegate that a touch down event was received on the format bar.
+    ///
     func formatBarTouchesBegan(_ formatBar: FormatBar)
+
+    /// Called when the overflow items in the format bar are either shown or hidden
+    /// as a result of the user tapping the toggle button.
+    ///
     func formatBar(_ formatBar: FormatBar, didChangeOverflowState overflowState: FormatBarOverflowState)
 }
