89e910a20d157a69fa9b57973fe9e9891b0cbc9c
Jorge Leandro Perez <jorge@lantean.co>
2017-07-21 15:38:38 -0300

    Implements UnsupportedHTML Tests

---
+++ AztecTests/TextKit/UnsupportedHTMLTests.swift
@@ -0,0 +1,65 @@
+import XCTest
+@testable import Aztec
+
+
+// MARK: - UnsupportedHTMLTests
+//
+class UnsupportedHTMLTests: XCTestCase {
+
+    /// Verifies that a UnsupportedHTML Instance can get properly serialized back and forth
+    ///
+    func testSnippetsGetProperlyEncodedAndDecoded() {
+        let unsupported = UnsupportedHTML()
+        unsupported.append(element: sampleElement)
+        unsupported.append(element: sampleElement)
+
+        let data = NSKeyedArchiver.archivedData(withRootObject: unsupported)
+        guard let restored = NSKeyedUnarchiver.unarchiveObject(with: data) as? UnsupportedHTML else {
+            XCTFail()
+            return
+        }
+
+        let elements = restored.elements
+        XCTAssert(elements.count == 2)
+
+        for element in elements {
+            XCTAssert(element.children == sampleChildren)
+
+            for (i, lhs) in sampleAttributes.enumerated() {
+                let rhs = element.attributes[i]
+                XCTAssert(lhs == rhs)
+            }
+
+// TODO: Figure out why this brekas?
+//            XCTAssert(element.attributes == sampleAttributes)
+        }
+    }
+}
+
+
+// MARK: - Helpers
+//
+private extension UnsupportedHTMLTests {
+    var sampleCSS: CSSProperty {
+        return CSSProperty(name: "text", value: "bold")
+    }
+
+    var sampleAttributes: [Attribute] {
+        return [
+            Attribute(name: "none", value: .none),
+            Attribute(name: "string", value: .string("value")),
+            Attribute(name: "style", value: .inlineCss([self.sampleCSS]))
+        ]
+    }
+
+    var sampleChildren: [Node] {
+        return [
+            TextNode(text: "Some Text"),
+            CommentNode(text: "Some Comment"),
+        ]
+    }
+
+    var sampleElement: ElementNode {
+        return ElementNode(name: "Test", attributes: self.sampleAttributes, children: self.sampleChildren)
+    }
+}
