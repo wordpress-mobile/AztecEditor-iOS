796b9f427a24f22a0c71831562779d0d920287a0
Jorge Leandro Perez <jorge@lantean.co>
2017-07-26 19:13:08 -0300

    ElementNodeTests: New Unit Test

--- AztecTests/HTML/ElementNodeTests.swift
+++ AztecTests/HTML/ElementNodeTests.swift
@@ -1,15 +1,22 @@
 import XCTest
 @testable import Aztec
 
+
+// MARK: ElementNodeTests
+//
 class ElementNodeTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    /// Verifies that two different ElementNode(s), with the same name, yet, different Children array, return
+    /// false when equality is checked.
+    ///
+    func testEqualityOperatorEffectivelyReturnsFalseWhenNodesDiffer() {
+        let text1 = TextNode(text: "First Children Here")
+        let style1 = ElementNode(name: "style", attributes: [], children: [text1])
+
+        let text2 = TextNode(text: "Second Child!")
+        let style2 = ElementNode(name: "style", attributes: [], children: [text2])
+
+        XCTAssert((style1 == style2) == false)
+        XCTAssert(style1 != style2)
     }
 }
