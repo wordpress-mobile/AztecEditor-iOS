104429f50b836798851b520ebee063f71418c27c
Sergio Estevao <sergioestevao@gmail.com>
2017-07-26 12:39:58 +0100

    Make selected assets empty after switching to full screen.

--- Example/WPMediaPicker/DemoViewController.m
+++ Example/WPMediaPicker/DemoViewController.m
@@ -237,6 +237,7 @@
     self.mediaPicker.dataSource = self.pickerDataSource;
     if (self.mediaInputViewController) {
         self.mediaPicker.mediaPicker.selectedAssets = self.mediaInputViewController.mediaPicker.selectedAssets;
+        self.mediaInputViewController.mediaPicker.selectedAssets = (NSArray<WPMediaAsset> *)@[];
     }
     self.mediaPicker.modalPresentationStyle = UIModalPresentationPopover;
     UIPopoverPresentationController *ppc = self.mediaPicker.popoverPresentationController;
