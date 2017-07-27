ab653d6b5216d65fc8418b37a5c832fd1562664a
Jorge Leandro Perez <jorge@lantean.co>
2017-07-27 12:43:22 -0300

    Reverting UnsupportedHTML Changes

--- Aztec/Classes/NSAttributedString/Styles/UnsupportedHTML.swift
+++ Aztec/Classes/NSAttributedString/Styles/UnsupportedHTML.swift
@@ -10,31 +10,34 @@ let UnsupportedHTMLAttributeName = "UnsupportedHTMLAttributeName"
 //
 class UnsupportedHTML: NSObject {
 
+    /// HTML Snippets not (natively) supported by the Editor (which will be re-serialized!!)
+    ///
+    private(set) var snippets = [String]()
+
     /// HTML Snippets not supported, converted back to their ElementNode representations
     ///
-    var elements = [ElementNode]()
-
-    /// Required Initializers
-    ///
-    public required convenience init?(coder aDecoder: NSCoder) {
-        self.init()
-
-        guard let snippets = aDecoder.decodeObject(forKey: Keys.elements) as? [String] else {
-            return
-        }
-
+    var elements: [ElementNode] {
         let converter = InHTMLConverter()
-        self.elements = snippets.flatMap { snippet in
+
+        return snippets.flatMap { snippet in
             // Strip the Root Node(s): Always return the first child element
             let root = converter.convert(snippet)
             return root.children.first as? ElementNode
         }
     }
 
+    /// Required Initializers
+    ///
+    public required convenience init?(coder aDecoder: NSCoder) {
+        self.init()
+        self.snippets = aDecoder.decodeObject(forKey: Keys.elements) as? [String] ?? []
+    }
+
     /// Appends the specified Element Representation
     ///
     func append(element: ElementNode) {
-        elements.append(element)
+        let snippet = OutHTMLConverter().convert(element)
+        snippets.append(snippet)
     }
 }
 
@@ -48,11 +51,6 @@ extension UnsupportedHTML: NSCoding {
     }
 
     open func encode(with aCoder: NSCoder) {
-        let converter = OutHTMLConverter()
-        let snippets = elements.map { element in
-            return converter.convert(element)
-        }
-
         aCoder.encode(snippets, forKey: Keys.elements)
     }
 }
