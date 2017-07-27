5a1f1ab17f5f3710e35e04bc924611ad419a4ad7
Jorge Leandro Perez <jorge@lantean.co>
2017-07-27 12:37:05 -0300

    ElementNodeTests: New Test

--- AztecTests/HTML/ElementNodeTests.swift
+++ AztecTests/HTML/ElementNodeTests.swift
@@ -2,7 +2,7 @@ import XCTest
 @testable import Aztec
 
 
-// MARK: ElementNodeTests
+// MARK: - ElementNodeTests
 //
 class ElementNodeTests: XCTestCase {
 
@@ -11,13 +11,30 @@ class ElementNodeTests: XCTestCase {
     ///
     func testEqualityOperatorEffectivelyReturnsFalseWhenNodesDiffer() {
         let text1 = TextNode(text: "First Children Here")
-        let style1 = ElementNode(name: "style", attributes: [], children: [text1])
+        let attribute1 = Attribute(name: "some", string: "value")
+        let style1 = ElementNode(name: "style", attributes: [attribute1], children: [text1])
 
         let text2 = TextNode(text: "Second Child!")
-        let style2 = ElementNode(name: "style", attributes: [], children: [text2])
+        let style2 = ElementNode(name: "style", attributes: [attribute1], children: [text2])
 
         XCTAssert((style1 == style2) == false)
         XCTAssert(style1 != style2)
         XCTAssert(style1 == style1)
+    }
+
+    /// Verifies that two different ElementNode(s) instances, with the same name and the exact same Children array
+    /// return true when equality is checked.
+    ///
+    func testEqualityOperatorEffectivelyReturnsTrueWhenNodesAreEqual() {
+        let text1 = TextNode(text: "First Children Here")
+        let attribute1 = Attribute(name: "some", string: "value")
+        let style1 = ElementNode(name: "style", attributes: [attribute1], children: [text1])
+
+        let text2 = TextNode(text: "First Children Here")
+        let attribute2 = Attribute(name: "some", string: "value")
+        let style2 = ElementNode(name: "style", attributes: [attribute2], children: [text2])
+
+        XCTAssert(style1 == style2)
+        XCTAssert(style1 !== style2)
     }
 }
