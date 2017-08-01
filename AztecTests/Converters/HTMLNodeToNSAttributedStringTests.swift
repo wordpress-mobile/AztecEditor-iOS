76a9e4e7f8f828cee22f017d3a7fe3e2a410de57
Jorge Leandro Perez <jorge@lantean.co>
2017-08-01 11:48:04 -0300

    HTMLNodeToNSAttributedStringTests: New Tests

--- AztecTests/Converters/HTMLNodeToNSAttributedStringTests.swift
+++ AztecTests/Converters/HTMLNodeToNSAttributedStringTests.swift
@@ -50,6 +50,51 @@ class HTMLNodeToNSAttributedStringTests: XCTestCase {
         XCTAssertEqual(restoredSpanAttribute1?.name, "class")
         XCTAssertEqual(restoredSpanAttribute1?.value.toString(), "first")
     }
+
+    /// Verifies that the DivFormatter effectively appends the DIV Element Representation, to the properties collection.
+    ///
+    func testHtmlDivFormatterEffectivelyAppendsNewDivProperty() {
+        let textNode = TextNode(text: "Ehlo World!")
+
+        let divAttr3 = Attribute(name: "class", value: .string("third"))
+        let divNode3 = ElementNode(type: .div, attributes: [divAttr3], children: [textNode])
+
+        let divAttr2 = Attribute(name: "class", value: .string("second"))
+        let divNode2 = ElementNode(type: .div, attributes: [divAttr2], children: [divNode3])
+
+        let divAttr1 = Attribute(name: "class", value: .string("first"))
+        let divNode1 = ElementNode(type: .div, attributes: [divAttr1], children: [divNode2])
+
+        // Convert
+        let output = attributedString(from: divNode1)
+
+        // Test!
+        var range = NSRange()
+        guard let paragraphStyle = output.attribute(NSParagraphStyleAttributeName, at: 0, effectiveRange: &range) as? ParagraphStyle else {
+            XCTFail()
+            return
+        }
+
+        XCTAssert(range.length == textNode.length())
+        XCTAssert(paragraphStyle.htmlDiv.count == 3)
+
+        guard case let .element(restoredDiv1) = paragraphStyle.htmlDiv[0].representation!.kind,
+            case let .element(restoredDiv2) = paragraphStyle.htmlDiv[1].representation!.kind,
+            case let .element(restoredDiv3) = paragraphStyle.htmlDiv[2].representation!.kind
+        else {
+            XCTFail()
+            return
+        }
+
+        XCTAssert(restoredDiv1.name == divNode1.name)
+        XCTAssert(restoredDiv1.attributes == [divAttr1])
+
+        XCTAssert(restoredDiv2.name == divNode2.name)
+        XCTAssert(restoredDiv2.attributes == [divAttr2])
+
+        XCTAssert(restoredDiv3.name == divNode3.name)
+        XCTAssert(restoredDiv3.attributes == [divAttr3])
+    }
 }
 
 
