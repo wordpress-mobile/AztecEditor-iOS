4c53ff47f5c3624f7b8c419917287057b903d1aa
Jorge Leandro Perez <jorge@lantean.co>
2017-09-08 15:12:25 -0300

    LayoutManager: Cleanup

--- Aztec/Classes/TextKit/LayoutManager.swift
+++ Aztec/Classes/TextKit/LayoutManager.swift
@@ -19,7 +19,7 @@ class LayoutManager: NSLayoutManager {
     ///
     var preBackgroundColor = UIColor(red: 243.0/255.0, green: 246.0/255.0, blue: 248.0/255.0, alpha: 1.0)
 
-    ///
+    /// Closure that is expected to return the TypingAttributes associated to the Extra Line Fragment
     ///
     var extraLineFragmentTypingAttributes: (() -> [String: Any])!
 
@@ -53,7 +53,7 @@ private extension LayoutManager {
 
         let characterRange = self.characterRange(forGlyphRange: glyphsToShow, actualGlyphRange: nil)
 
-        // Draw blockquotes
+        // Draw: Blockquotes
         textStorage.enumerateAttribute(NSParagraphStyleAttributeName, in: characterRange, options: []) { (object, range, stop) in
             guard let paragraphStyle = object as? ParagraphStyle, !paragraphStyle.blockquotes.isEmpty else {
                 return
@@ -63,40 +63,43 @@ private extension LayoutManager {
             let blockquoteGlyphRange = glyphRange(forCharacterRange: range, actualCharacterRange: nil)
 
             enumerateLineFragments(forGlyphRange: blockquoteGlyphRange) { (rect, usedRect, textContainer, glyphRange, stop) in
-
                 let lineRange = self.characterRange(forGlyphRange: glyphRange, actualGlyphRange: nil)
                 let lineCharacters = textStorage.attributedSubstring(from: lineRange).string
-                let blockquoteRect = self.blockquoteRect(origin: origin, lineRect: rect, lineCharacters: lineCharacters, blockquoteIndent: blockquoteIndent)
+                let lineEndsParagraph = lineCharacters.isEndOfParagraph(before: lineCharacters.endIndex)
+                let blockquoteRect = self.blockquoteRect(origin: origin, lineRect: rect, blockquoteIndent: blockquoteIndent, lineEndsParagraph: lineEndsParagraph)
 
                 self.drawBlockquote(in: blockquoteRect.integral, with: context)
             }
         }
 
-        if extraLineFragmentRect != .zero {
-            let typingAttributes = extraLineFragmentTypingAttributes()
-
-            guard let paragraphStyle = typingAttributes[NSParagraphStyleAttributeName] as? ParagraphStyle, !paragraphStyle.blockquotes.isEmpty else {
-                return
-            }
-
-            let blockquoteIndent = paragraphStyle.blockquoteIndent
-            // let extraLineRect = extraLineFragmentRect.offsetBy(dx: origin.x, dy: origin.y)
-            let blockquoteRect = self.blockquoteRect(origin: origin, lineRect: extraLineFragmentRect, lineCharacters: "", blockquoteIndent: blockquoteIndent)
-            self.drawBlockquote(in: blockquoteRect.integral, with: context)
+        // Draw: Extra Line Fragment
+        guard extraLineFragmentRect != .zero else {
+            return
         }
+
+        let typingAttributes = extraLineFragmentTypingAttributes()
+        guard let paragraphStyle = typingAttributes[NSParagraphStyleAttributeName] as? ParagraphStyle, !paragraphStyle.blockquotes.isEmpty else {
+            return
+        }
+
+        let extraIndent = paragraphStyle.blockquoteIndent
+        let extraRect = blockquoteRect(origin: origin, lineRect: extraLineFragmentRect, blockquoteIndent: extraIndent, lineEndsParagraph: false)
+
+        drawBlockquote(in: extraRect.integral, with: context)
     }
+
 
     /// Returns the Rect in which the Blockquote should be rendered.
     ///
     /// - Parameters:
     ///     - origin: Origin of coordinates
     ///     - lineRect: Line Fragment's Rect
-    ///     - lineCharacters: Substring representing the current line
     ///     - blockquoteIndent: Blockquote Indentation Level for the current lineFragment
+    ///     - lineEndsParagraph: Indicates if the current blockquote line is the end of a Paragraph
     ///
     /// - Returns: Rect in which we should render the blockquote.
     ///
-    private func blockquoteRect(origin: CGPoint, lineRect: CGRect, lineCharacters: String, blockquoteIndent: CGFloat) -> CGRect {
+    private func blockquoteRect(origin: CGPoint, lineRect: CGRect, blockquoteIndent: CGFloat, lineEndsParagraph: Bool) -> CGRect {
         var blockquoteRect = lineRect.offsetBy(dx: origin.x, dy: origin.y)
         guard blockquoteIndent != 0 else {
             return blockquoteRect
@@ -107,12 +110,13 @@ private extension LayoutManager {
         blockquoteRect.size.width -= paddingWidth
 
         // Ref. Issue #645: Cheking if we this a middle line inside a blockquote paragraph
-        if lineCharacters.isEndOfParagraph(before: lineCharacters.endIndex) {
+        if lineEndsParagraph {
             blockquoteRect.size.height -= Metrics.paragraphSpacing * 0.5
         }
 
         return blockquoteRect
     }
+
 
     /// Draws a single Blockquote Line Fragment, in the specified Rectangle, using a given Graphics Context.
     ///
@@ -123,24 +127,6 @@ private extension LayoutManager {
         let borderRect = CGRect(origin: rect.origin, size: CGSize(width: 2, height: rect.height))
         blockquoteBorderColor.setFill()
         context.fill(borderRect)
-    }
-
-    ///
-    ///
-    private func mustDrawExtraLineFragment() -> Bool {
-        guard extraLineFragmentRect.height != 0 else {
-            return false
-        }
-
-        guard let extraTypingAttributes = extraLineFragmentTypingAttributes?() else {
-            return false
-        }
-
-        guard let extraStyle = extraTypingAttributes[NSParagraphStyleAttributeName] as? ParagraphStyle else {
-            return false
-        }
-
-        return !extraStyle.blockquotes.isEmpty
     }
 }
 
@@ -177,6 +163,7 @@ private extension LayoutManager {
 
     }
 
+
     /// Draws a single HTML Pre Line Fragment, in the specified Rectangle, using a given Graphics Context.
     ///
     private func drawHTMLPre(in rect: CGRect, with context: CGContext) {
@@ -184,6 +171,7 @@ private extension LayoutManager {
         context.fill(rect)
     }
 }
+
 
 // MARK: - Lists Helpers
 //
@@ -214,6 +202,7 @@ private extension LayoutManager {
             drawItem(number: markerNumber, in: markerRect, from: list, using: paragraphStyle, at: enclosingRange.location)
         }
     }
+
 
     /// Returns the Rect for the MarkerItem at the specified Range + Origin, within a given ParagraphStyle.
     ///
