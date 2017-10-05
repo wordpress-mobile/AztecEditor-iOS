fb425e715428a1f4adaa9c007316224599c1990b
Jorge Leandro Perez <jorge@lantean.co>
2017-10-05 15:16:51 -0300

    TextView: Updates workaround

--- Aztec/Classes/TextKit/TextView.swift
+++ Aztec/Classes/TextKit/TextView.swift
@@ -457,20 +457,9 @@ open class TextView: UITextView {
 
         ensureRemovalOfParagraphStylesBeforeRemovingCharacter(at: selectedRange)
 
-        // WORKAROUND: iOS 11 introduced an issue that's causing UITextView to lose it's typing
-        // attributes under certain circumstances.  This workaround is analog to the one introduced in
-        /// the `insertText` call.
-        //
-        // Issue: https://github.com/wordpress-mobile/AztecEditor-iOS/issues/749
-        //
-        let workaroundTypingAttributes = typingAttributes
-
         super.deleteBackward()
 
-        // WORKAROUND: this line is related to the workaround above.
-        //
-        typingAttributes = workaroundTypingAttributes
-
+        ensureTypingAttributesAreValid()
         ensureRemovalOfParagraphAttributesWhenPressingBackspaceAndEmptyingTheDocument()
         ensureCursorRedraw(afterEditing: deletedString.string)
         delegate?.textViewDidChange?(self)
@@ -976,6 +965,23 @@ open class TextView: UITextView {
             self.selectedRange = pristine
             self.typingAttributes = beforeTypingAttributes
         }
+    }
+
+
+    // WORKAROUND: iOS 11 introduced an issue that's causing UITextView to lose it's typing
+    // attributes under certain circumstances. This method will determine the Typing Attributes based on
+    /// the TextStorage attributes, whenever possible.
+    ///
+    /// Issue: https://github.com/wordpress-mobile/AztecEditor-iOS/issues/749
+    ///
+    private func ensureTypingAttributesAreValid() {
+        let document = textStorage.string
+        guard selectedRange.location == document.characters.count else {
+            return
+        }
+
+        let previousLocation = max(selectedRange.location - 1, 0)
+        typingAttributes = textStorage.attributes(at: previousLocation, effectiveRange: nil)
     }
 
 
