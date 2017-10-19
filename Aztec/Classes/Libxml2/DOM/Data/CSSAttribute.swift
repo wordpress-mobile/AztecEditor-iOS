b7ceb9a9f413c7037acf4bcc0ac6809efbb36436
Jorge Leandro Perez <jorge@lantean.co>
2017-10-19 13:32:25 -0300

    CSSAttribute: Pure Swift!

--- Aztec/Classes/Libxml2/DOM/Data/CSSAttribute.swift
+++ Aztec/Classes/Libxml2/DOM/Data/CSSAttribute.swift
@@ -3,81 +3,77 @@ import Foundation
 
 // MARK: - CSSAttribute
 //
-class CSSAttribute: NSObject, CustomReflectable, NSCoding {
+class CSSAttribute: Codable {
 
+    /// Attribute Name
+    ///
     let name: String
+
+    /// Attribute Value, if any!
+    ///
     let value: String?
+
 
     // MARK: - Initializers
 
     init(name: String, value: String? = nil) {
-        self.name = name
-        self.value = value
+        self.name = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        self.value = value?.trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
     convenience init?(for string: String) {
         let components = string.components(separatedBy: CSSParser.keyValueSeparator)
-
-        guard let name = components.first?.trimmingCharacters(in: .whitespacesAndNewlines),
-            let value = components.last?.trimmingCharacters(in: .whitespacesAndNewlines),
-            components.count == 2 else {
-                return nil
+        guard let name = components.first, let value = components.last, components.count == 2 else {
+            return nil
         }
 
         self.init(name: name, value: value)
     }
 
-    // MARK: - NSCoding
 
-    required init?(coder aDecoder: NSCoder) {
-        guard let name = aDecoder.decodeObject(forKey: #keyPath(name)) as? String,
-            let value = aDecoder.decodeObject(forKey: #keyPath(value)) as? String? else {
-                return nil
-        }
-
-        self.name = name.trimmingCharacters(in: .whitespacesAndNewlines)
-        self.value = value?.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
-
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(name, forKey: #keyPath(name))
-        aCoder.encode(value, forKey: #keyPath(value))
-    }
-
-    // MARK: - CustomReflectable
-
-    public var customMirror: Mirror {
-        get {
-            return Mirror(self, children: ["name": name, "value": value ?? ""])
-        }
-    }
-
-    // MARK: - Hashable
-
-    override var hashValue: Int {
-
-        guard let value = value else {
-            return name.hashValue
-        }
-
-        return name.hashValue ^ value.hashValue
-    }
-
-    // MARK: - String Representation
+    // MARK: - Public Methods
 
     func toString() -> String {
-
         guard let value = value else {
             return name
         }
 
         return name + CSSParser.keyValueSeparator + " " + value.trimmingCharacters(in: .whitespacesAndNewlines)
     }
+}
 
-    // MARK: - Equatable
 
-    override func isEqual(_ object: Any?) -> Bool {
+// MARK: - CustomReflectable Conformance
+//
+extension CSSAttribute: CustomReflectable {
 
+    public var customMirror: Mirror {
+        get {
+            return Mirror(self, children: ["name": name, "value": value ?? ""])
+        }
+    }
+}
+
+
+// MARK: - Hashable Conformance
+//
+extension CSSAttribute: Hashable {
+
+    var hashValue: Int {
+        guard let value = value else {
+            return name.hashValue
+        }
+
+        return name.hashValue ^ value.hashValue
+    }
+}
+
+
+// MARK: - Equatable Conformance
+//
+extension CSSAttribute: Equatable {
+
+    func isEqual(_ object: Any?) -> Bool {
         guard let rightValue = object as? CSSAttribute else {
             return false
         }
@@ -89,4 +85,3 @@ class CSSAttribute: NSObject, CustomReflectable, NSCoding {
         return leftValue.name == rightValue.name && leftValue.value == rightValue.value
     }
 }
-
