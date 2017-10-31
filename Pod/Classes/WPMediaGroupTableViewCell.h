2f247609b6f3ac61529e0ca0819d78159cf67a4c
Sergio Estevao <sergioestevao@gmail.com>
2017-10-31 11:06:16 +0000

    Add a group identifier to allow better match of cells to groups.

--- Pod/Classes/WPMediaGroupTableViewCell.h
+++ Pod/Classes/WPMediaGroupTableViewCell.h
@@ -6,5 +6,6 @@
 @property (nonatomic, strong) UILabel *titleLabel;
 @property (nonatomic, strong) UILabel *countLabel;
 @property (nonatomic, strong) UIColor *posterBackgroundColor UI_APPEARANCE_SELECTOR;
+@property (nonatomic, strong) NSString *groupIdentifier;
 
 @end
