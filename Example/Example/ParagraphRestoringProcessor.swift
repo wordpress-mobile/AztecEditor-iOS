94d7db92c326d48dc5d2ab95de337b894f156941
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-10-31 14:23:11 -0300

    Fixes some code.

--- Example/Example/ParagraphRestoringProcessor.swift
+++ Example/Example/ParagraphRestoringProcessor.swift
@@ -3,94 +3,11 @@ import Foundation
 
 class ParagraphRestoringProcessor: HTMLTreeProcessor {
     func process(_ rootNode: RootNode) -> RootNode {
-        
-        var index = 0
-        
-        removeNewlinesFromChildren(of: rootNode)
-        
-        var wrapStartIndex = 0
-        var wrapEndIndex = 0
-
-        // First clean BRs:
-        // - Converts single \n at root level to <br/>
-        // - Converts double \n at root level to </p>
-        // - Converts any \n non at root level to <br/>
-        //
-        // cleanNewlines()
-        
-        while index < rootNode.children.count {
-            let node = rootNode.children[index]
-            
-            guard var textNode = node as? TextNode else {
-                index += 1
-                continue
-            }
-            /*
-            if let range = textNode.text().rangeOfCharacter(from: .newlines) {
-                if let nextRange = textNode.text().rangeOfCharacter(from: .newlines),
-                    nextRange.lowerBound == range.upperBound {
-                    
-                    // Paragraph ending
-                    
-                    split(textNode)
-                    
-                    let nodesToWrap = rootNode.children.dropFirst(startIndex).dropLast(endIndex)
-                } else {
-                    // Single line break
-                    
-                    let leftText = textNode.text().substring(to: range.lowerBound)
-                    let rightText = textNode.text().substring(from: range.upperBound)
-                    
-                    let leftTextNode = TextNode(text: leftText)
-                    let brNode = ElementNode(type: .br)
-                    let rightTextNode = TextNode(text: rightText)
-                    
-                    rootNode.children.remove(at: index)
-                    rootNode.children.insert(rightTextNode, at: index)
-                    rootNode.children.insert(brNode, at: index)
-                    rootNode.children.insert(leftTextNode, at: index)
-                }
-            }
-            
-            guard let elementNode = node as? ElementNode else {
-            }
-            
-            guard !elementNode.isNodeType(.br) else {
-                // Process
-            }
-            
-            guard elementNode.isBlockLevelElement() else {
-                rootNode.children.remove(at: index)
-                nodesToWrapInParagraph.append(node)
-                continue
-            }
-            
-            
-            if nodesToWrapInParagraph.count > 0 {
-                let paragraph = wrapInParagraph(nodesToWrapInParagraph)
-                
-                rootNode.children.insert(paragraph, at: index)
-                nodesToWrapInParagraph.removeAll()
-            }
-            
-            continue
-            
-            guard elementNode.standardName != .br else {
-                continue
-            } */
-            // If <br>, with a <br> coming up next, wrap all pending nodes
-            // Otherwise add the node to the list of nodes to wrap in a paragraph
-        }
-
         return rootNode
-    }
-    
-    private func wrapInParagraph(_ nodes: [Node]) -> ElementNode {
-        return ElementNode(type: .p, attributes: [], children: nodes)
     }
 }
 
-extension ParagraphRemovingProcessor {
+extension ParagraphRestoringProcessor {
     
     func removeNewlinesFromChildren(of rootNode: RootNode) {
         removeNewlinesFromChildren(of: rootNode, restoreParagraphs: true)
@@ -120,6 +37,10 @@ extension ParagraphRemovingProcessor {
             }
             
             textToProcess = textToProcess + textNode.text()
+        }
+        
+        for child in processedNodes {
+            child.parent = element
         }
         
         element.children = processedNodes
