c6f3164a8225679d8c44386f8f0c6cb2620a3729
Diego Rey Mendez <diegoreymendez@gmail.com>
2017-10-31 14:19:01 -0300

    First version of the ParagraphRestoringProcessor.

--- Example/Example/ParagraphRemovingProcessor.swift
+++ Example/Example/ParagraphRemovingProcessor.swift
@@ -3,8 +3,13 @@ import Foundation
 
 class ParagraphRemovingProcessor: HTMLTreeProcessor {
     func process(_ rootNode: RootNode) -> RootNode {
-        // All nodes at the root level that are not block nodes will be wrapped by paragraphs.
-        var nodesToWrapInParagraph = [Node]()
+        
+        var index = 0
+        
+        removeNewlinesFromChildren(of: rootNode)
+        
+        var wrapStartIndex = 0
+        var wrapEndIndex = 0
 
         // First clean BRs:
         // - Converts single \n at root level to <br/>
@@ -13,12 +18,38 @@ class ParagraphRemovingProcessor: HTMLTreeProcessor {
         //
         // cleanNewlines()
         
-        for (index, node) in rootNode.children.enumerated() {
+        while index < rootNode.children.count {
+            let node = rootNode.children[index]
             
-            guard let textNode = node as? TextNode else {
-                rootNode.children.remove(at: index)
-                nodesToWrapInParagraph.append(node)
+            guard var textNode = node as? TextNode else {
+                index += 1
                 continue
+            }
+            /*
+            if let range = textNode.text().rangeOfCharacter(from: .newlines) {
+                if let nextRange = textNode.text().rangeOfCharacter(from: .newlines),
+                    nextRange.lowerBound == range.upperBound {
+                    
+                    // Paragraph ending
+                    
+                    split(textNode)
+                    
+                    let nodesToWrap = rootNode.children.dropFirst(startIndex).dropLast(endIndex)
+                } else {
+                    // Single line break
+                    
+                    let leftText = textNode.text().substring(to: range.lowerBound)
+                    let rightText = textNode.text().substring(from: range.upperBound)
+                    
+                    let leftTextNode = TextNode(text: leftText)
+                    let brNode = ElementNode(type: .br)
+                    let rightTextNode = TextNode(text: rightText)
+                    
+                    rootNode.children.remove(at: index)
+                    rootNode.children.insert(rightTextNode, at: index)
+                    rootNode.children.insert(brNode, at: index)
+                    rootNode.children.insert(leftTextNode, at: index)
+                }
             }
             
             guard let elementNode = node as? ElementNode else {
@@ -46,7 +77,7 @@ class ParagraphRemovingProcessor: HTMLTreeProcessor {
             
             guard elementNode.standardName != .br else {
                 continue
-            }
+            } */
             // If <br>, with a <br> coming up next, wrap all pending nodes
             // Otherwise add the node to the list of nodes to wrap in a paragraph
         }
@@ -57,10 +88,79 @@ class ParagraphRemovingProcessor: HTMLTreeProcessor {
     private func wrapInParagraph(_ nodes: [Node]) -> ElementNode {
         return ElementNode(type: .p, attributes: [], children: nodes)
     }
+}
+
+extension ParagraphRemovingProcessor {
     
-    private func replaceLineBreaks(in element: ElementNode) {
-            elementNode.isNodeType(.br) else {
+    func removeNewlinesFromChildren(of rootNode: RootNode) {
+        removeNewlinesFromChildren(of: rootNode, restoreParagraphs: true)
+    }
+    
+    // Recursive
+    private func removeNewlinesFromChildren(of element: ElementNode, restoreParagraphs: Bool) {
+        
+        var processedNodes = [Node]()
+        var textToProcess = ""
+        
+        for child in element.children {
+            guard let textNode = child as? TextNode else {
+                if textToProcess.characters.count > 0 {
+                    let nodes = self.nodes(for: textToProcess, restoreParagraphs: restoreParagraphs)
+                    
+                    processedNodes.append(contentsOf: nodes)
+                    textToProcess = ""
+                }
                 
+                if let element = child as? ElementNode {
+                    removeNewlinesFromChildren(of: element, restoreParagraphs: false)
+                }
+                
+                processedNodes.append(child)
+                continue
+            }
+            
+            textToProcess = textToProcess + textNode.text()
+        }
+        
+        element.children = processedNodes
+    }
+    
+    private func nodes(for text: String, restoreParagraphs: Bool) -> [Node] {
+        if restoreParagraphs {
+            return nodesRestoringBreaksAndParagraphs(for: text)
+        } else {
+            return nodesRestoringBreaks(for: text)
         }
     }
-}
+    
+    private func nodesRestoringBreaksAndParagraphs(for text: String) -> [Node] {
+        var nodes = [Node]()
+        let paragraphs = text.components(separatedBy: "\n\n")
+        
+        for paragraph in paragraphs {
+            let children = nodesRestoringBreaks(for: paragraph)
+            let paragraph = ElementNode(type: .p, attributes: [], children: children)
+            
+            nodes.append(paragraph)
+        }
+        
+        return nodes
+    }
+    
+    private func nodesRestoringBreaks(for text: String) -> [Node] {
+        var nodes = [Node]()
+        let lines = text.components(separatedBy: "\n")
+        
+        for (index, line) in lines.enumerated() {
+            if index > 0 {
+                nodes.append(ElementNode(type: .br))
+            }
+            
+            let textNode = TextNode(text: line)
+            
+            nodes.append(textNode)
+        }
+        
+        return nodes
+    }
+ }
