d9c4e1f367f5f5b6ee62b9a8ca813324ef0e9a62
Jorge Leandro Perez <jorge@lantean.co>
2017-12-12 11:47:37 -0300

    FigureElementConverter: `supports` specific implementation

--- Aztec/Classes/ElementConverters/Implementations/FigureElementConverter.swift
+++ Aztec/Classes/ElementConverters/Implementations/FigureElementConverter.swift
@@ -6,26 +6,32 @@ import UIKit
 class FigureElementConverter: ElementConverter {
 
     func convert(from element: ElementNode, inheritedAttributes: [AttributedStringKey: Any]) -> NSAttributedString {
-        guard element.isNodeType(.figure),
-            element.children.count == 2,
-            let imgElement = element.firstChild(ofType: .img),
+        assert(supports(element: element))
+
+        // Extract the Image + Figcaption Elements
+        //
+        guard let imgElement = element.firstChild(ofType: .img),
             let captionElement = element.firstChild(ofType: .figcaption)
         else {
             fatalError()
         }
 
-        let imageConverter = ImageElementConverter()
-        let imageString = imageConverter.convert(from: imgElement, inheritedAttributes: inheritedAttributes)
-
-        guard let attachment = imageString.attribute(.attachment, at: 0, effectiveRange: nil) as? ImageAttachment else {
+        // Convert the Image Element
+        //
+        let output = ImageElementConverter().convert(from: imgElement, inheritedAttributes: inheritedAttributes)
+        guard let imageAttachment = output.attribute(.attachment, at: 0, effectiveRange: nil) as? ImageAttachment else {
             fatalError()
         }
 
+        // Serialize the Figcaption:
+        // We're wrapping the Figcaption's children within a figcaption, so that the `<figcaption>` element itself doesn't get mapped
+        // as UnknownHTML
+        //
         let wrappedCaptionChildren = RootNode(children: captionElement.children)
         let serializer = AttributedStringSerializer(defaultAttributes: inheritedAttributes)
-        attachment.caption = serializer.serialize(wrappedCaptionChildren)
+        imageAttachment.caption = serializer.serialize(wrappedCaptionChildren)
 
-        return imageString
+        return output
     }
 
     func specialString(for element: ElementNode) -> String {
@@ -34,5 +40,16 @@ class FigureElementConverter: ElementConverter {
 
     func extraAttributes(for representation: HTMLRepresentation) -> [AttributedStringKey: Any]? {
         return [.hrHtmlRepresentation: representation]
+    }
+
+    /// Indicates if the current ElementNode is supported, or not. For now, at least, only the following Figure is supported:
+    ///
+    /// `<figure><img/><figcaption></figcaption></figure>`
+    ///
+    func supports(element: ElementNode) -> Bool {
+        return element.isNodeType(.figure) &&
+            element.children.count == 2 &&
+            element.firstChild(ofType: .img) != nil &&
+            element.firstChild(ofType: .figcaption) != nil
     }
 }
