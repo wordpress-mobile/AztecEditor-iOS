abfd6d379d27fb5b8318604119a74962dab8da96
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2017-12-19 10:40:15 +0700

    Disable animations when running UI tests

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -7,6 +7,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let args = ProcessInfo().arguments
+        
+        for arg in args {
+            if arg == "NoAnimations" {
+                UIView.setAnimationsEnabled(false)
+                application.windows.first?.layer.speed = MAXFLOAT
+
+            }
+        }
         // Override point for customization after application launch.
         return true
     }
