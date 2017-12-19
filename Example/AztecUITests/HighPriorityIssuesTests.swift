f29be6dde8b700e018e8e90d5f7cf042d63c5708
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2017-12-19 10:33:43 +0700

    testLongTitle expects title to expand on multiple lines

--- Example/AztecUITests/HighPriorityIssuesTests.swift
+++ Example/AztecUITests/HighPriorityIssuesTests.swift
@@ -25,6 +25,7 @@ class HighPriorityIssuesTests: XCTestCase {
     
     // Github issue https://github.com/wordpress-mobile/AztecEditor-iOS/issues/385
     func testLongTitle() {
+        let longTitle = "long title in a galaxy not so far away"
         // Title heigh contains of actual textfield height + bottom line.
         // 16px - is the height of that bottom line. Its not changing with different font sizes
         let titleTextView = app.textViews[elementStringIDs.titleTextField]
@@ -34,13 +35,13 @@ class HighPriorityIssuesTests: XCTestCase {
         
         // TODO: Move it into EditorPage
         if isIPhone() {
-            titleTextView.typeText("very very very very very very long title in a galaxy not so far away")
+            titleTextView.typeText(String(repeating: "very ", count: 6) + longTitle)
         } else {
-            titleTextView.typeText("very very very very very very long title in a galaxy not so far away very very very very very very long title in a galaxy not so far away")
+            titleTextView.typeText(String(repeating: "very ", count: 20) + longTitle)
         }
         
         let twoLineTitleHeight = titleTextView.frame.height
-        XCTAssert(twoLineTitleHeight - oneLineTitleHeight == titleLineHeight )
+        XCTAssert(twoLineTitleHeight - oneLineTitleHeight >= titleLineHeight )
     }
     
     // Github issue https://github.com/wordpress-mobile/AztecEditor-iOS/issues/675
