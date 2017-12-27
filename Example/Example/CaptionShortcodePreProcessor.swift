a5c6f1aa185b00135ecfe1502fffb2e09f64fae8
Jorge Leandro Perez <jorge@lantean.co>
2017-12-27 16:45:56 -0300

    CaptionShortcodePreProcessor: Reimplementing Processor

--- Example/Example/CaptionShortcodePreProcessor.swift
+++ Example/Example/CaptionShortcodePreProcessor.swift
@@ -1,27 +1,44 @@
 import Aztec
 import Foundation
 
+
+// MARK: - CaptionShortcodePreProcessor: Converts [caption] shortcode into a <figure><img><figcaption> structure.
+//
 class CaptionShortcodePreProcessor: ShortcodeProcessor {
 
+    struct Constants {
+        static let captionTag = "caption"
+    }
+
     init() {
-        super.init(tag: "caption") { shortcode -> String in
-            var html = "<div data-shortcode=\"caption\" "
-
-            for (key, value) in shortcode.attributes.named {
-                html += "\(key)=\"\(value)\" "
+        super.init(tag: Constants.captionTag) { shortcode in
+            guard let payloadText = shortcode.content else {
+                return nil
             }
 
-            for value in shortcode.attributes.unamed {
-                html += "\(value) "
+            /// Parse the Shortcode's Payload: We expect an [Img, Text, ...]
+            ///
+            let payloadNode = HTMLParser().parse(payloadText)
+            guard let imageNode = payloadNode.firstChild(ofType: .img), payloadNode.children.count >= 2 else {
+                return nil
             }
 
-            if let content = shortcode.content {
-                html += ">" + content + "</div>"
-            } else {
-                html += "/>"
-            }
-            
-            return html
+            /// Figcaption: Figure Children (minus) the image
+            ///
+            var caption = Set(payloadNode.children)
+            caption.remove(imageNode)
+
+            let figcaptionNode = ElementNode(type: .figcaption, attributes: [], children: Array(caption))
+
+            /// Figure: Image + Figcaption! Woo!
+            ///
+            let figure = ElementNode(type: .figure, attributes: [], children: [imageNode, figcaptionNode])
+
+            /// Final Step: Serialize back to string.
+            /// This is expected to produce a `<figure><img<figcaption/></figure>` snippet.
+            ///
+            let serializer = DefaultHTMLSerializer()
+            return serializer.serialize(figure)
         }
     }
 }
