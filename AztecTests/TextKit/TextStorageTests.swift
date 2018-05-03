a3eaf87d82fd7bd033450cac29a74f908a21fd8e
Sergio Estevao <sergioestevao@gmail.com>
2018-05-03 10:59:24 +0100

    Add test for comments surrounding block elements.

--- AztecTests/TextKit/TextStorageTests.swift
+++ AztecTests/TextKit/TextStorageTests.swift
@@ -431,5 +431,20 @@ class TextStorageTests: XCTestCase {
         XCTAssertEqual(expectedHTML, outputHTML)
     }
 
+    func testCommentSurroundingBlockLevelElements() {
+        let elementsToTest: [StandardElementType] = [.p, .pre, .div, .h2, .h3, .h4, .h5, .h6]
+
+        for element in elementsToTest {
+            let html = "<!-- comment --><\(element) class=\"custom_hr\">Some content</\(element)><!-- comment -->"
+            let expectedHTML = "<p><!-- comment --></p><\(element) class=\"custom_hr\">Some content</\(element)><p><!-- comment --></p>"
+            let defaultAttributes: [NSAttributedStringKey: Any] = [.font: UIFont.systemFont(ofSize: 14),
+                                                                   .paragraphStyle: ParagraphStyle.default]
+            storage.setHTML(html, defaultAttributes: defaultAttributes)
+            let outputHTML = storage.getHTML(serializer: serializer)
+
+            XCTAssertEqual(expectedHTML, outputHTML)
+        }
+    }
+
 
 }
