2fc672576693e5935946c813e3bde0a115f212e9
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-05-18 09:43:17 -0300

    Reorganizes the code a bit and adds documentation.

--- Aztec/Classes/Plugin/Plugin.swift
+++ Aztec/Classes/Plugin/Plugin.swift
@@ -6,15 +6,36 @@ open class Plugin {
     
     public init() {}
     
+    /// Method plugins can use to execute extra code when loaded.
+    ///
     public func loaded() {}
     
-    open func process(inputHTML html: String) -> String { return html }
-    open func process(outputHTML html: String) -> String { return html }
-    open func process(inputHTMLTree tree: RootNode) { return }
-    open func process(outputHTMLTree tree: RootNode) { return }
+    // MARK: - Input processing
     
-    open func elementConverters() -> [ElementConverter] { return [] }
+    /// Processes an HTML string right before parsing it to convert it into a nodes tree in
+    /// the input conversion process.
+    ///
+    open func process(inputHTML html: String) -> String { return html }
+    
+    /// Processes a nodes tree right after it's been parsed from a string, and before finalizing
+    /// the input conversion process.
+    ///
+    open func process(inputHTMLTree tree: RootNode) { return }
+    
+    // MARK: - Output Processing
+    
+    /// Processes an HTML string right after converting it from a nodes tree in the output
+    /// conversion process.
+    ///
+    open func process(outputHTML html: String) -> String { return html }
+    
+    /// Processes a nodes tree right before it'll bee converted to a string, and before finalizing
+    /// the output conversion process.
+    ///
+    open func process(outputHTMLTree tree: RootNode) { return }
 
+    // MARK: - Equatable
+    
     public static func ==(lhs: Plugin, rhs: Plugin) -> Bool {
         return type(of: lhs) == type(of: rhs)
     }
--- Aztec/Classes/Plugin/PluginsManager.swift
+++ Aztec/Classes/Plugin/PluginsManager.swift
@@ -1,11 +1,15 @@
 import Foundation
 
+/// This class managed the loading and provides an execution interface for plugins.
+///
 class PluginsManager {
     
     // MARK: - Plugin Loading
     
     private(set) var plugins = [Plugin]()
     
+    /// Loads a plugins.
+    ///
     public func load(_ plugin: Plugin) {
         guard !plugins.contains(where: { $0 == plugin }) else {
             assertionFailure()
@@ -16,26 +20,40 @@ class PluginsManager {
         plugin.loaded()
     }
     
-    // MARK: - Processing
+    // MARK: - Input Processing
     
+    /// Processes an HTML string right before parsing it to convert it into a nodes tree in
+    /// the input conversion process.
+    ///
     func process(inputHTML html: String) -> String {
         return plugins.reduce(html) { (html, plugin) -> String in
             plugin.process(inputHTML: html)
         }
     }
     
-    func process(outputHTML html: String) -> String {
-        return plugins.reduce(html) { (html, plugin) -> String in
-            plugin.process(outputHTML: html)
-        }
-    }
-    
+    /// Processes a nodes tree right after it's been parsed from a string, and before finalizing
+    /// the input conversion process.
+    ///
     func process(inputHTMLTree tree: RootNode) {
         for plugin in plugins {
             plugin.process(inputHTMLTree: tree)
         }
     }
     
+    // MARK: - Output Processing
+    
+    /// Processes an HTML string right after converting it from a nodes tree in the output
+    /// conversion process.
+    ///
+    func process(outputHTML html: String) -> String {
+        return plugins.reduce(html) { (html, plugin) -> String in
+            plugin.process(outputHTML: html)
+        }
+    }
+    
+    /// Processes a nodes tree right before it'll bee converted to a string, and before finalizing
+    /// the output conversion process.
+    ///
     func process(outputHTMLTree tree: RootNode) {
         for plugin in plugins {
             plugin.process(outputHTMLTree: tree)
