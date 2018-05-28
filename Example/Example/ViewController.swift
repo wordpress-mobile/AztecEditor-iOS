72b87540901ea11135ab887ae35e0cef588e673b
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-05-28 16:24:25 -0300

    Enabled the empty Gutenberg demo.

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UITableViewController
             ),
             DemoSection(title: "WordPressEditor (Calypso & Gutenberg)", rows: [
                 DemoRow(title: "Gutenberg Demo", action: { self.showEditorDemo(filename: "gutenberg", wordPressMode: true) }),
-                DemoRow(title: "Empty Demo", action: { self.showEditorDemo() })
+                DemoRow(title: "Empty Demo", action: { self.showEditorDemo(wordPressMode: true) })
                 ]
             ),
         ]
