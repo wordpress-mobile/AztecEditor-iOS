89039ea888f656fce6224795c75448e81b2a34cd
Sergio Estevao <sergioestevao@gmail.com>
2018-06-01 11:21:06 +0100

    Fix parsing of poster attribute on videos.

--- Aztec/Classes/ElementConverters/Implementations/VideoElementConverter.swift
+++ Aztec/Classes/ElementConverters/Implementations/VideoElementConverter.swift
@@ -49,9 +49,9 @@ class VideoElementConverter: AttachmentElementConverter {
         }
 
         let posterURL: URL?
-        let classAttribute = element.attributes.first(where: { $0.name == "class" })
+        let posterAttribute = element.attributes.first(where: { $0.name == "poster" })
         
-        if let urlString = classAttribute?.value.toString() {
+        if let urlString = posterAttribute?.value.toString() {
             posterURL = URL(string: urlString)
             extraAttributes.removeValue(forKey: "poster")
         } else {
