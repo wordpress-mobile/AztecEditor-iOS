35ca1a9758b2f3c69c8cc5c07ea18fc86f69e8fc
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-06-08 16:53:14 -0300

    Adds unit tests.

--- WordPressEditor/WordPressEditorTests/GalleryElementConverterTests.swift
+++ WordPressEditor/WordPressEditorTests/GalleryElementConverterTests.swift
@@ -33,4 +33,44 @@ class GalleryElementConverterTests: XCTestCase {
         XCTAssertNil(gallery.order)
         XCTAssertNil(gallery.orderBy)
     }
+    
+    func testGalleryElementConverterWithOnlyOrder() {
+        let orderValues = ["asc", "Desc"]
+        
+        for orderValue in orderValues {
+            let expected = GalleryAttachment.Order(rawValue: orderValue)
+            
+            let attributes = [Attribute(name: "orderby", value: .string(orderValue))]
+            let element = ElementNode(type: .gallery, attributes: attributes)
+            
+            let (gallery, _) = converter.convert(element, inheriting: [:]) { (node, attributes) -> NSAttributedString in
+                return NSAttributedString()
+            }
+            
+            XCTAssertNil(gallery.columns)
+            XCTAssertNil(gallery.ids)
+            XCTAssertEqual(gallery.order, expected)
+            XCTAssertNil(gallery.orderBy)
+        }
+    }
+    
+    func testGalleryElementConverterWithOnlyOrderBy() {
+        let orderByValues = ["menu_order", "title", "post_date", "rand"]
+        
+        for orderByValue in orderByValues {
+            let expected = GalleryAttachment.OrderBy(rawValue: orderByValue)
+            
+            let attributes = [Attribute(name: "orderby", value: .string(orderByValue))]
+            let element = ElementNode(type: .gallery, attributes: attributes)
+            
+            let (gallery, _) = converter.convert(element, inheriting: [:]) { (node, attributes) -> NSAttributedString in
+                return NSAttributedString()
+            }
+            
+            XCTAssertNil(gallery.columns)
+            XCTAssertNil(gallery.ids)
+            XCTAssertNil(gallery.order)
+            XCTAssertEqual(gallery.orderBy, expected)
+        }
+    }
 }
