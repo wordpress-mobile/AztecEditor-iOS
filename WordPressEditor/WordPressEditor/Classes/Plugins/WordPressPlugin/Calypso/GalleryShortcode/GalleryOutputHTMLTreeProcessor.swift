447a8fd0d653d78670e3943805101c4c733d6431
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-06-13 13:04:20 -0300

    Fixes a bug in GalleryOutputHTMLTreeProcessor.swift

--- WordPressEditor/WordPressEditor/Classes/Plugins/WordPressPlugin/Calypso/GalleryShortcode/GalleryOutputHTMLTreeProcessor.swift
+++ WordPressEditor/WordPressEditor/Classes/Plugins/WordPressPlugin/Calypso/GalleryShortcode/GalleryOutputHTMLTreeProcessor.swift
@@ -8,10 +8,19 @@ public class GalleryOutputHTMLTreeProcessor: HTMLTreeProcessor {
     public init() {}
     
     public func process(_ rootNode: RootNode) {
-        rootNode.children = rootNode.children.map ({ (node) -> Node in
-            guard let element = node as? ElementNode,
-                element.type == .gallery else {
-                    return node
+        rootNode.children = process(rootNode.children)
+    }
+    
+    func process(_ nodes: [Node]) -> [Node] {
+        return nodes.map ({ (node) -> Node in
+            guard let element = node as? ElementNode else {
+                return node
+            }
+            
+            element.children = process(element.children)
+            
+            guard element.type == .gallery else {
+                return element
             }
             
             return converter.convert(element)
