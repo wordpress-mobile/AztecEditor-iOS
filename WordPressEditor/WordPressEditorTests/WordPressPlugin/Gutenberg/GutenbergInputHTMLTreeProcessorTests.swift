a40d8a77b29c3b2615c631c5de42c454e4db2d6e
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-09-10 14:56:50 -0300

    Documents the newly added unit test.

--- WordPressEditor/WordPressEditorTests/WordPressPlugin/Gutenberg/GutenbergInputHTMLTreeProcessorTests.swift
+++ WordPressEditor/WordPressEditorTests/WordPressPlugin/Gutenberg/GutenbergInputHTMLTreeProcessorTests.swift
@@ -181,7 +181,11 @@ class GutenbergInputHTMLTreeProcessorTests: XCTestCase {
         }))
     }
     
-    func testLostText() {
+    /// There was an issue with Gutenberg posts losing edits.  This automated test makes sure the issues we found don't regress.
+    ///
+    /// https://github.com/wordpress-mobile/AztecEditor-iOS/issues/1052
+    ///
+    func testDataLossWhenProcessing() {
         let input = "<!-- wp:paragraph -->a<!-- /wp:paragraph -->\n<!-- wp:image -->\n<figure class=\"wp-block-image alignright\"><img src=\"https://wordpress.org/gutenberg/files/2017/12/Dropcap.png\" height=234 alt=\"\" width=312></figure>\nThis text will be lost<!-- /wp:image -->"
         
         let rootNode = parser.parse(input)
