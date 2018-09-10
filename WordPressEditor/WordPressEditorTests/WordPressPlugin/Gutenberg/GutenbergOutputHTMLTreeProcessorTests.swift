83e50674a8a700f91555ae7d275329c201350df5
Diego Rey Mendez <diegoreymendez@gmail.com>
2018-09-10 18:49:49 -0300

    Adds a unit test and fixes what seems to be a Swift bug with defer{}.

--- WordPressEditor/WordPressEditorTests/WordPressPlugin/Gutenberg/GutenbergOutputHTMLTreeProcessorTests.swift
+++ WordPressEditor/WordPressEditorTests/WordPressPlugin/Gutenberg/GutenbergOutputHTMLTreeProcessorTests.swift
@@ -29,4 +29,23 @@ class GutenbergOutputHTMLTreeProcessorTests: XCTestCase {
         processor.process(rootNode)
         XCTAssertTrue(rootNode.rawText().contains("This text will be lost"))
     }
+    
+    /// There was an issue with Gutenberg posts losing edits.  This automated test makes sure the issues we found don't regress.
+    ///
+    /// https://github.com/wordpress-mobile/AztecEditor-iOS/issues/1052
+    ///
+    func testDataLossWhenProcessing2() {
+        
+        let paragraphBefore = ElementNode(type: .gutenblock, attributes: [], children: [TextNode(text: "a")])
+        let paragraphAfter = ElementNode(type: .gutenblock, attributes: [], children: [TextNode(text: "b")])
+        
+        let commentNode = CommentNode(text: "wp:latestposts /")
+        let gutenpackAttribute = encoder.selfClosingAttribute(for: commentNode)
+        let gutenpack = ElementNode(type: .gutenpack, attributes: [gutenpackAttribute], children: [])
+        
+        let paragraph = ElementNode(type: .p, attributes: [], children: [paragraphBefore, gutenpack, paragraphAfter])
+        let rootNode = RootNode(children: [paragraph])
+        
+        processor.process(rootNode)
+    }
 }
