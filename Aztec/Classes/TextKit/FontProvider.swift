b155e7da0594a718d75b7c851ba6a1cbb55fab44
Sergio Estevao <sergioestevao@gmail.com>
2019-10-08 21:53:44 +0100

    Make font properties public

--- Aztec/Classes/TextKit/FontProvider.swift
+++ Aztec/Classes/TextKit/FontProvider.swift
@@ -9,7 +9,7 @@ public class FontProvider {
 
     static var shared = FontProvider()
 
-    lazy var monospaceFont: UIFont = {
+    public lazy var monospaceFont: UIFont = {
         let baseFont = UIFont(descriptor:UIFontDescriptor(name: "Menlo", size: 14), size:14)
         let font: UIFont
         if #available(iOS 11.0, *) {
@@ -20,7 +20,7 @@ public class FontProvider {
         return font
     }()
 
-    lazy var defaultFont: UIFont = {
+    public lazy var defaultFont: UIFont = {
         let baseFont = UIFont.systemFont(ofSize: 14)
         let font: UIFont
         if #available(iOS 11.0, *) {
