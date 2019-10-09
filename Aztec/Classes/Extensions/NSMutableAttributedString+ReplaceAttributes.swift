3d0c2e8177f5468710f1b0a6cbb667fed93d5fe3
Sergio Estevao <sergioestevao@gmail.com>
2019-10-09 09:54:13 +0100

    Improve replaceFont code and add some documentation.

--- Aztec/Classes/Extensions/NSMutableAttributedString+ReplaceAttributes.swift
+++ Aztec/Classes/Extensions/NSMutableAttributedString+ReplaceAttributes.swift
@@ -3,19 +3,23 @@ import UIKit
 
 public extension NSMutableAttributedString {
 
+    /// Replace all instances of the .font attribute that belong to the same family for the new font, trying to keep the same symbolic traits
+    /// - Parameter font: the original font to be replaced
+    /// - Parameter newFont: the new font to use.
     func replace(font: UIFont, with newFont: UIFont) {
-        let fullRange = NSRange(location: 0, length: self.length)
+        let fullRange = NSRange(location: 0, length: length)
 
-        self.beginEditing()
-        self.enumerateAttributes(in: fullRange, options: []) { (attributes, subrange, stop) in
-            if let currentFont = attributes[.font] as? UIFont, currentFont.familyName == font.familyName {
-                var replacementFont = newFont
-                if let fontDescriptor = newFont.fontDescriptor.withSymbolicTraits(currentFont.fontDescriptor.symbolicTraits) {
-                    replacementFont = UIFont(descriptor: fontDescriptor, size: currentFont.pointSize)
-                }
-                self.addAttribute(.font, value: replacementFont, range: subrange)
+        beginEditing()
+        enumerateAttributes(in: fullRange, options: []) { (attributes, subrange, stop) in
+            guard let currentFont = attributes[.font] as? UIFont, currentFont.familyName == font.familyName else {
+                return
             }
+            var replacementFont = newFont
+            if let fontDescriptor = newFont.fontDescriptor.withSymbolicTraits(currentFont.fontDescriptor.symbolicTraits) {
+                replacementFont = UIFont(descriptor: fontDescriptor, size: currentFont.pointSize)
+            }
+            addAttribute(.font, value: replacementFont, range: subrange)
         }
-        self.endEditing()
+        endEditing()
     }
 }
