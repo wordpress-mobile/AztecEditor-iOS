78d52eccc5fd826435b91cb5ec1da184ed41056e
Sergio Estevao <sergioestevao@gmail.com>
2020-01-08 17:29:08 +0000

    Create a formatter for each call.

--- Aztec/Classes/TextKit/Configuration.swift
+++ Aztec/Classes/TextKit/Configuration.swift
@@ -4,11 +4,13 @@ public final class Configuration {
 
     public static var headersWithBoldTrait = false
 
-    static var defaultBoldFormatter: AttributeFormatter = {
-        if headersWithBoldTrait {
-            return BoldWithShadowForHeadingFormatter()
-        } else {
-            return BoldFormatter()
+    static var defaultBoldFormatter: AttributeFormatter {
+        get {
+            if headersWithBoldTrait {
+                return BoldWithShadowForHeadingFormatter()
+            } else {
+                return BoldFormatter()
+            }
         }
-    } ()
+    }
 }
