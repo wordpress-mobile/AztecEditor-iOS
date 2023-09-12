1bd24f9415d3ee550cb6d6d88c01458b8cc56300
Tanner W. Stokes <tanner.stokes@automattic.com>
2023-09-12 12:34:55 -0400

    Add tests for paragraphRange crash.

--- AztecTests/Extensions/NSAttributedStringParagraphRangeTests.swift
+++ AztecTests/Extensions/NSAttributedStringParagraphRangeTests.swift
@@ -46,4 +46,54 @@ class NSAttributedStringParagraphRangeTests: XCTestCase {
         XCTAssertEqual(ranges.first?.rangeExcludingParagraphSeparator, expectedRangeWithoutSeparator)
         XCTAssertEqual(ranges.first?.rangeIncludingParagraphSeparator, expectedRangeWithSeparator)
     }
+
+    /// Tests that `paragraphRange(for:)` with a paragraph separator character
+    /// returns the correct `NSRange`.
+    ///
+    /// This test was added due to an iOS 17 crash when calling String.paragraphRange(for: range)
+    /// on a single paragraph separator character.
+    ///
+    func testParagraphRangeWorkWithParagraphSeparator() {
+        let attributedString = NSAttributedString(string: "\u{2029}")
+        let range = NSRange(location: 0, length: 1)
+        let expectedRange = NSRange(location: 0, length: 1)
+
+        let paragraphRange = attributedString.paragraphRange(for: range)
+
+        XCTAssertEqual(paragraphRange, expectedRange)
+    }
+
+    /// Tests that `paragraphRanges(intersecting:)` with a paragraph separator character
+    /// returns the correct `[NSRange]`.
+    ///
+    /// This test was added due to an iOS 17 crash when calling String.paragraphRange(for: range)
+    /// on a single paragraph separator character.
+    ///
+    func testParagraphRangesWorkWithParagraphSeparator() {
+        let attributedString = NSAttributedString(string: "\u{2029}")
+        let range = NSRange(location: 0, length: 1)
+        let expectedRange = NSRange(location: 0, length: 1)
+
+        let ranges = attributedString.paragraphRanges(intersecting: range, includeParagraphSeparator: true)
+
+        XCTAssertEqual(ranges.first!, expectedRange)
+    }
+
+    /// Tests that `paragraphRanges(intersecting:)` with a paragraph separator character
+    /// returns the correct `ParagraphRange`.
+    ///
+    /// This test was added due to an iOS 17 crash when calling String.paragraphRange(for: range)
+    /// on a single paragraph separator character.
+    ///
+    func testParagraphRangesWorkWithAndWithoutParagraphSeparator() {
+        let attributedString = NSAttributedString(string: "\u{2029}")
+        let range = NSRange(location: 0, length: 1)
+        let expectedRangeWithoutSeparator = NSRange(location: 0, length: 0)
+        let expectedRangeWithSeparator = NSRange(location: 0, length: 1)
+
+        let ranges = attributedString.paragraphRanges(intersecting: range)
+
+        XCTAssertEqual(ranges.first!.rangeIncludingParagraphSeparator, expectedRangeWithSeparator)
+        XCTAssertEqual(ranges.first!.rangeExcludingParagraphSeparator, expectedRangeWithoutSeparator)
+    }
 }
