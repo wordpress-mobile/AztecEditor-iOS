4711da4fe44e0932ea13e0204d16890ec15cfaef
Gerardo <gerardo.pacheco@automattic.com>
2024-01-12 19:08:50 +0100

    Update MarkFormatter to support incoming color changes

--- Aztec/Classes/Formatters/Implementations/MarkFormatter.swift
+++ Aztec/Classes/Formatters/Implementations/MarkFormatter.swift
@@ -4,25 +4,41 @@ import UIKit
 class MarkFormatter: AttributeFormatter {
 
     var placeholderAttributes: [NSAttributedString.Key: Any]?
-
+    var textColor: String?
+    var defaulTextColor: UIColor?
+    
     func applicationRange(for range: NSRange, in text: NSAttributedString) -> NSRange {
         return range
     }
 
     func apply(to attributes: [NSAttributedString.Key: Any], andStore representation: HTMLRepresentation?) -> [NSAttributedString.Key: Any] {
-       var resultingAttributes = attributes
+        var resultingAttributes = attributes
+        let colorStyle = CSSAttribute(name: "color", value: textColor)
+        let backgroundColorStyle = CSSAttribute(name: "background-color", value: "rgba(0, 0, 0, 0)")
 
-        var representationToUse = HTMLRepresentation(for: .element(HTMLElementRepresentation.init(name: "mark", attributes: [])))
+        let styleAttribute = Attribute(type: .style, value: .inlineCss([backgroundColorStyle, colorStyle]))
+        let classAttribute = Attribute(type: .class, value: .string("has-inline-color"))
+
+        // Setting the HTML representation
+        var representationToUse = HTMLRepresentation(for: .element(HTMLElementRepresentation.init(name: "mark", attributes: [styleAttribute, classAttribute])))
         if let requestedRepresentation = representation {
             representationToUse = requestedRepresentation
         }
         resultingAttributes[.markHtmlRepresentation] = representationToUse
+        
+        if (textColor != nil) {
+            resultingAttributes[.foregroundColor] = UIColor(hexString: textColor!)
+        }
 
         return resultingAttributes
     }
 
     func remove(from attributes: [NSAttributedString.Key: Any]) -> [NSAttributedString.Key: Any] {
         var resultingAttributes = attributes
+        
+        if (defaulTextColor != nil) {
+            resultingAttributes[.foregroundColor] = defaulTextColor
+        }
 
         resultingAttributes.removeValue(forKey: .markHtmlRepresentation)
         return resultingAttributes
