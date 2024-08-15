fc8861a90e17bc9faeab205f3d0faa457ee3e5ee
Tony Li <tony.li@automattic.com>
2024-08-16 11:46:26 +1200

    Fix Xcode's Fix-It code suggestions

--- Example/Example/UnknownEditorViewController.swift
+++ Example/Example/UnknownEditorViewController.swift
@@ -9,16 +9,16 @@ class UnknownEditorViewController: UIViewController {
 
     /// Save Bar Button
     ///
-    fileprivate(set) var saveButton: UIBarButtonItem = {
+    fileprivate(set) lazy var saveButton: UIBarButtonItem = {
         let saveTitle = NSLocalizedString("Save", comment: "Save Action")
-        return UIBarButtonItem(title: saveTitle, style: .plain, target: UnknownEditorViewController.self, action: #selector(saveWasPressed))
+        return UIBarButtonItem(title: saveTitle, style: .plain, target: self, action: #selector(saveWasPressed))
     }()
 
     /// Cancel Bar Button
     ///
-    fileprivate(set) var cancelButton: UIBarButtonItem = {
+    fileprivate(set) lazy var cancelButton: UIBarButtonItem = {
         let cancelTitle = NSLocalizedString("Cancel", comment: "Cancel Action")
-        return UIBarButtonItem(title: cancelTitle, style: .plain, target: UnknownEditorViewController.self, action: #selector(cancelWasPressed))
+        return UIBarButtonItem(title: cancelTitle, style: .plain, target: self, action: #selector(cancelWasPressed))
     }()
 
     /// HTML Editor
